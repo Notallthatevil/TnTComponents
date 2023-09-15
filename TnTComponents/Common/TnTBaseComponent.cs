5276141774581b397057f6396e8e36d5cea5326a
Nate Tripp <nate.tripp13448@gmail.com>
2023-09-15 12:33:10 -0600

    Initial setup of SideNav

---
+++ TnTComponents/Common/TnTBaseComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Common {
+    public class TnTBaseComponent : ComponentBase {
+
+        [Parameter]
+        public string Class { get; set; } = string.Empty;
+    }
+}
