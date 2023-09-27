d00aba0fc4e8e0aec6cd704e906289c6f3452f70
Nate Tripp <nate.tripp13448@gmail.com>
2023-09-27 08:57:09 -0600

    Reworked buttons to match Card style

---
+++ TnTComponents/Enum/ButtonType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Enum {
+    public enum ButtonType {
+        Default,
+        Flat,
+        Filled,
+        Outline
+    }
+}
