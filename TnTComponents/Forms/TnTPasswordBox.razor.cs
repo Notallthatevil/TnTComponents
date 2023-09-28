2e5c86ac5a905e58dd89d8e659da6b0b5c65a0d2
Nate Tripp <nate.tripp13448@gmail.com>
2023-09-28 13:44:44 -0600

    Updated styling and simplified reveal

---
+++ TnTComponents/Forms/TnTPasswordBox.razor.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace TnTComponents.Forms;
+public partial class TnTPasswordBox {
+    protected override string InputType => PasswordRevealed ? "text" : "password";
+
+    [Parameter]
+    public bool AllowPasswordReveal { get; set; } = true;
+
+    private bool PasswordRevealed { get; set; } = false;
+}
+
