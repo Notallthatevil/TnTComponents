d4d10ed728854e8a66f633ac977598e7b147f6b2
Nate Tripp <ntripp@workfirstcasualty.com>
2023-10-02 15:11:45 -0600

    Major upgrades to DropDown

---
+++ TnTComponents/Common/Ext/JSRuntimeExt.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Common.Ext;
+internal static class JSRuntimeExt {
+    public static async Task<double> ConvertRemToPixels(this IJSRuntime jsRuntime, double rem) {
+        return await jsRuntime.InvokeAsync<double>("TnTComponents.remToPx", rem);
+    }
+
+    public static async Task<ElementBoundingRect> GetElementBoundingRectForComponent(this IJSRuntime jsRuntime, ElementReference element) {
+        return await jsRuntime.InvokeAsync<ElementBoundingRect>("TnTComponents.getBoundingRect", element);
+    }
+
+    public static async Task<double> GetWindowHeight(this IJSRuntime jsRuntime) {
+        return await jsRuntime.InvokeAsync<double>("TnTComponents.getWindowHeight");
+    }
+    public static async Task RemoveElementFocus(this IJSRuntime jsRuntime, ElementReference element) {
+        await jsRuntime.InvokeVoidAsync("TnTComponents.removeFocus", element);
+    }
+
+}
+
---
+++ TnTComponents/wwwroot/scripts/TnTComponents.js
@@ -0,0 +1,29 @@
+﻿window.TnTComponents = {
+    getBoundingRect: (element) => { return element.getBoundingClientRect(); },
+    getWindowHeight: () => { return window.innerHeight; },
+
+    MediaCallbacks: {},
+    MediaCallback: function (query, instance) {
+        if (instance) {
+            function callback(event) {
+                instance.invokeMethodAsync('OnChanged', event.matches)
+            };
+            var query = matchMedia(query);
+            this.MediaCallbacks[instance._id] = function () {
+                query.removeListener(callback);
+            }
+            query.addListener(callback);
+            return query.matches;
+        } else {
+            instance = query;
+            if (this.MediaCallbacks[instance._id]) {
+                this.MediaCallbacks[instance._id]();
+                delete this.MediaCallbacks[instance._id];
+            }
+        }
+    },
+
+    removeFocus: (element) => { element.blur(); },
+
+    remToPx: (rem) => { return rem * parseFloat(getComputedStyle(document.documentElement).fontSize); }
+}
