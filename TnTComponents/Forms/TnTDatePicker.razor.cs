2218cb56a00eed9dcba8a8a80db695b1058a066d
Nate Tripp <ntripp@workfirstcasualty.com>
2023-10-30 11:48:59 -0600

    Flushed out date field

--- TnTComponents/Forms/TnTDatePicker.razor.cs
+++ TnTComponents/Forms/TnTDatePicker.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using TnTComponents.Enum;
 
@@ -11,23 +12,63 @@ public partial class TnTDatePicker {
     [Parameter]
     public DateOnly DefaultDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
+    [Parameter]
+    public DateOnly MaxDate { get; set; }
+
+    [Parameter]
+    public DateOnly MinDate { get; set; }
+
+    [Parameter]
+    public IReadOnlySet<DateOnly> DisabledDates { get; set; } = new HashSet<DateOnly>();
+
     private readonly ICollection<int> _months = Enumerable.Range(1, 12).ToList();
-    private readonly IReadOnlySet<int> _disabledMonths = new HashSet<int>();
-    private readonly ICollection<int> _years = Enumerable.Range(2000, 2050).ToList();
+    private readonly HashSet<int> _disabledMonths = [];
+    private ICollection<int> _years = default!;
 
-    private int _dropdownMonths;
-    private int _dropdownYears;
+    private int _dropdownMonth;
+    private int _dropdownYearValue;
+    private int _dropdownYear {
+        get => _dropdownYearValue;
+        set {
+            _dropdownYearValue = value;
+            if (value == MaxDate.Year) {
+                _disabledMonths.Clear();
+                for (var i = MaxDate.Month + 1; i <= 12; ++i) {
+                    _disabledMonths.Add(i);
+                }
+            }
+            else if (value == MinDate.Year) {
+                _disabledMonths.Clear();
+                for (var i = MinDate.Month - 1; i >= 0; --i) {
+                    _disabledMonths.Add(i);
+                }
+            }
+        }
+    }
 
 
-    public TnTDatePicker() {
-        _dropdownMonths = DefaultDate.Month;
-        _dropdownYears = DefaultDate.Year;
+
+    protected override void OnInitialized() {
+        _dropdownMonth = DefaultDate.Month;
+        _dropdownYear = DefaultDate.Year;
+
+        if (MaxDate == default) {
+            MaxDate = DefaultDate.AddYears(1);
+        }
+
+        if (MinDate == default) {
+            MinDate = DefaultDate.AddYears(-1);
+        }
+
+        _years = Enumerable.Range(MinDate.Year, (MaxDate.Year - MinDate.Year) + 1).ToList();
+
+        base.OnInitialized();
     }
 
 
     /// <inheritdoc/>
-    protected override string? FormatValueAsString(DateOnly value) {
-        return value.ToString(Format);
+    protected override string? FormatValueAsString(DateOnly? value) {
+        return value?.ToString(Format);
     }
 
     protected override string GetCssClass() {
@@ -39,93 +80,48 @@ public partial class TnTDatePicker {
         await base.OnFocusOutAsync(e);
     }
 
-    private void PrevMonth() { }
-    private void NextMonth() { }
-    //[Parameter]
-    //public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+    private void SelectDate(DateOnly selectedDate) {
+        if (selectedDate >= MinDate && selectedDate <= MaxDate && !DisabledDates.Contains(selectedDate)) {
+            if (selectedDate.Month == _dropdownMonth && selectedDate.Year == _dropdownYear) {
+                Value = selectedDate;
+                //_currentMonthYear = new DateOnly(selectedDate.Year, selectedDate.Month, 1);
+                //await OnChange(new ChangeEventArgs { Value = Value });
+            }
+            else if (selectedDate.Year == _dropdownYear) {
+                if (selectedDate.Month < _dropdownMonth) {
+                    PrevPage();
+                }
+                else {
+                    NextPage();
+                }
+            }
+            else if (selectedDate.Year < _dropdownYear) {
+                PrevPage();
+            }
+            else {
+                NextPage();
+            }
+        }
+    }
 
-    //[Parameter]
-    //public DateOnly MinDate { get; set; } = DateOnly.FromDateTime(DateTime.Now.AddYears(-50));
+    private void NextPage() {
+        if (CanShowNextMonth()) {
+            var nextMonthPage = new DateOnly(_dropdownYear, _dropdownMonth, 1).AddMonths(1);
+            _dropdownYear = nextMonthPage.Year;
+            _dropdownMonth = nextMonthPage.Month;
+        }
+    }
 
-    //[Parameter]
-    //public DateOnly MaxDate { get; set; } = DateOnly.FromDateTime(DateTime.Now.AddYears(50));
-
-    //[Parameter]
-    //public IReadOnlySet<DateOnly>? DisabledDates { get; set; }
-
-    //private DateOnly _currentMonthYear;
-    //private int _selectedMonth { get => _currentMonthYear.Month; set => UpdateCurrentDateView(new DateOnly(_selectedYear, value, 1)); }
-    //private int _selectedYear { get => _currentMonthYear.Year; set => UpdateCurrentDateView(new DateOnly(value, _selectedMonth, 1)); }
-    //private ICollection<int> _months = Enumerable.Range(1, 12).ToArray();
-    //private ICollection<int> _years = default!;
-
-    //private HashSet<int> _disabledMonths = new HashSet<int>();
-
-    //protected override void OnInitialized() {
-    //    if (StartDate < MinDate) {
-    //        throw new ArgumentException($"{nameof(StartDate)} cannot be less then {nameof(MinDate)}");
-    //    }
-
-    //    if (StartDate > MaxDate) {
-    //        throw new ArgumentException($"{nameof(StartDate)} cannot be greater then {nameof(MaxDate)}");
-    //    }
-
-    //    _selectedMonth = StartDate.Month;
-    //    _selectedYear = StartDate.Year;
-
-    //    _years = Enumerable.Range(MinDate.Year, (MaxDate.Year - MinDate.Year) + 1).ToArray();
-
-    //    if (string.IsNullOrWhiteSpace(Format)) {
-    //        Format = "yyyy-MM-dd";
-    //    }
-
-    //    base.OnInitialized();
-    //}
-
-    //protected override string GetCssClass() {
-    //    return $"{base.GetCssClass()} date-picker";
-    //}
-
-    //protected override async Task OnChange(ChangeEventArgs e) {
-    //    await ValueChanged.InvokeAsync((DateOnly?)e?.Value);
-    //}
-
-    //private async Task SelectDate(DateOnly selectedDate) {
-    //    if (selectedDate >= MinDate && selectedDate <= MaxDate && !DisabledDates.Contains(selectedDate)) {
-    //        if (selectedDate.Month == _currentMonthYear.Month && selectedDate.Year == _currentMonthYear.Year) {
-    //            Value = selectedDate;
-    //            _currentMonthYear = new DateOnly(selectedDate.Year, selectedDate.Month, 1);
-    //            await OnChange(new ChangeEventArgs { Value = Value });
-    //        }
-    //        else if (selectedDate < _currentMonthYear) {
-    //            PrevPage();
-    //            return;
-    //        }
-    //        else if (selectedDate > _currentMonthYear) {
-    //            NextPage();
-    //            return;
-    //        }
-    //    }
-    //}
-
-    //private void NextPage() {
-    //    var nextMonth = _currentMonthYear.AddMonths(1);
-    //    var nextMonthPage = new DateOnly(nextMonth.Year, nextMonth.Month, 1);
-    //    if (nextMonth <= MaxDate) {
-    //        UpdateCurrentDateView(nextMonth);
-    //    }
-    //}
-
-    //private void PrevPage() {
-    //    var prevMonth = _currentMonthYear.AddMonths(-1);
-    //    var prevPageDate = new DateOnly(prevMonth.Year, prevMonth.Month, DateTime.DaysInMonth(prevMonth.Year, prevMonth.Month));
-    //    if (prevPageDate >= MinDate) {
-    //        UpdateCurrentDateView(prevMonth);
-    //    }
-    //}
+    private void PrevPage() {
+        if (CanShowPrevMonth()) {
+            var prevMonthPage = new DateOnly(_dropdownYear, _dropdownMonth, 1).AddMonths(-1);
+            _dropdownYear = prevMonthPage.Year;
+            _dropdownMonth = prevMonthPage.Month;
+        }
+    }
 
     private DateOnly GetStartDate() {
-        var startDate = new DateTime(_dropdownYears, _dropdownMonths, 1);
+        var startDate = new DateTime(_dropdownYear, _dropdownMonth, 1);
         return DateOnly.FromDateTime(startDate.AddDays(-(double)startDate.DayOfWeek));
     }
 
@@ -135,7 +131,7 @@ public partial class TnTDatePicker {
             strBuilder.Append("current-date ");
         }
 
-        if (date.Month != _dropdownMonths) {
+        if (date.Month != _dropdownMonth) {
             strBuilder.Append("out-of-month ");
         }
 
@@ -143,35 +139,19 @@ public partial class TnTDatePicker {
             strBuilder.Append("selected ");
         }
 
-        //if (date > MaxDate || date < MinDate || DisabledDates.Contains(date)) {
-        //    strBuilder.Append("disabled ");
-        //}
+        if (date > MaxDate || date < MinDate || DisabledDates.Contains(date)) {
+            strBuilder.Append("disabled ");
+        }
 
         return strBuilder.ToString();
     }
 
-    //private void UpdateCurrentDateView(DateOnly newDate) {
-    //    if (newDate < MinDate) {
-    //        _currentMonthYear = MinDate;
-    //    }
-    //    else if (newDate > MaxDate) {
-    //        _currentMonthYear = MaxDate;
-    //    }
-    //    else {
-    //        _currentMonthYear = newDate;
-    //    }
 
-    //    _disabledMonths.Clear();
-    //    if (_currentMonthYear.Year == MinDate.Year) {
-    //        foreach (var month in Enumerable.Range(1, MinDate.Month - 1)) {
-    //            _disabledMonths.Add(month);
-    //        }
-    //    }
+    private bool CanShowPrevMonth() {
+        return new DateOnly(_dropdownYear, _dropdownMonth, 1).AddDays(-1) >= MinDate;
 
-    //    if (_currentMonthYear.Year == MaxDate.Year) {
-    //        foreach (var month in Enumerable.Range(MaxDate.Month + 1, 12 - MaxDate.Month)) {
-    //            _disabledMonths.Add(month);
-    //        }
-    //    }
-    //}
+    }
+    private bool CanShowNextMonth() {
+        return new DateOnly(_dropdownYear, _dropdownMonth, 1).AddMonths(1) <= MaxDate;
+    }
 }
