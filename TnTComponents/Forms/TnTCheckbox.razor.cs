915f2448793945cf1fad88c9167398807c181064
Nate Tripp <ntripp@workfirstcasualty.com>
2023-11-27 11:24:25 -0700

    Implmented switchs

--- TnTComponents/Forms/TnTCheckbox.razor.cs
+++ TnTComponents/Forms/TnTCheckbox.razor.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace TnTComponents.Forms;
 
 public partial class TnTCheckbox {
-
+    protected virtual bool Switch { get; }
     protected override bool TryParseValueFromString(string? value, out bool result, [NotNullWhen(false)] out string? validationErrorMessage)
         => throw new NotSupportedException($"This component does not parse string inputs. Bind to the '{nameof(CurrentValue)}' property, not '{nameof(CurrentValueAsString)}'.");
 }
--- TnTComponents/Forms/TnTSwitch.cs
+++ TnTComponents/Forms/TnTSwitch.cs
@@ -3,8 +3,5 @@ using Microsoft.AspNetCore.Components;
 namespace TnTComponents.Forms;
 
 public class TnTSwitch : TnTCheckbox {
-
-    protected override void OnInitialized() {
-        base.OnInitialized();
-    }
+    protected override bool Switch => true;
 }
