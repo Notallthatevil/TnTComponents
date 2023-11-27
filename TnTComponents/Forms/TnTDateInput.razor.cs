4188f883618d0786ddec7957ff4f881eccfa8d5e
Nate Tripp <ntripp@workfirstcasualty.com>
2023-11-27 12:34:18 -0700

    Reworked Date field

---
+++ TnTComponents/Forms/TnTDateInput.razor.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TnTComponents.Forms;
+public partial class TnTDateInput {
+
+    [Parameter]
+    public DateTime? MinDate { get; set; }
+    [Parameter]
+    public DateTime? MaxDate { get; set; }
+
+    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out DateTime? result, [NotNullWhen(false)] out string? validationErrorMessage)
+        => throw new NotSupportedException($"This component does not parse string inputs. Bind to the '{nameof(CurrentValue)}' property, not '{nameof(CurrentValueAsString)}'.");
+}
