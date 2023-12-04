7b137f3d8da4780a14eb72ae0979089247211e1f
Nate Tripp <ntripp@workfirstcasualty.com>
2023-12-04 14:57:07 -0700

    Added circular progress bar

--- TnTComponents/ProgressIndicator.razor.cs
+++ TnTComponents/ProgressIndicator.razor.cs
@@ -4,20 +4,21 @@ using TnTComponents.Enum;
 namespace TnTComponents;
 
 public partial class ProgressIndicator {
-    [Parameter]
-    public override string? Class { get; set; } = "tnt-progress-indicator";
-
-    [Parameter]
-    public double? Value{ get; set; }
-
-    [Parameter]
-    public double Max { get; set; }
 
     [Parameter]
     public ProgressAppearance Appearance { get; set; }
 
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public override string? Class { get; set; } = "tnt-progress-indicator";
+
+    [Parameter]
+    public double Max { get; set; }
+
+    [Parameter]
+    public double? Value { get; set; }
 
     public override string GetClass() => base.GetClass() + " " + Appearance.ToString().ToLower();
 }
