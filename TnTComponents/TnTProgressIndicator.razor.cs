54bccd7c9c1a79f11b4cd582a89a6ae9e03b92db
Nate Tripp <nate.tripp13448@gmail.com>
2023-12-05 10:02:20 -0700

    Added ring progress indicator

--- TnTComponents/TnTProgressIndicator.razor.cs
+++ TnTComponents/TnTProgressIndicator.razor.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Components;
+using System.Text;
 using TnTComponents.Enum;
 
 namespace TnTComponents;
 
-public partial class ProgressIndicator {
+public partial class TnTProgressIndicator {
 
     [Parameter]
     public ProgressAppearance Appearance { get; set; }
@@ -15,10 +16,21 @@ public partial class ProgressIndicator {
     public override string? Class { get; set; } = "tnt-progress-indicator";
 
     [Parameter]
-    public double Max { get; set; }
+    public double Max { get; set; } = 100.0;
 
     [Parameter]
     public double? Value { get; set; }
 
     public override string GetClass() => base.GetClass() + " " + Appearance.ToString().ToLower();
+
+    private string GetStyle() {
+        var stringBuilder = new StringBuilder(Style);
+
+        if (Value is not null) {
+            var deg = 360 * (Value / Max);
+            stringBuilder.Append($"background: conic-gradient(from 0deg, currentColor {deg}deg, transparent {deg}deg);");
+        }
+
+        return stringBuilder.ToString();
+    }
 }
