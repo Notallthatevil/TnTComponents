7713f2e2520f6ee537456a6cc1a049b2a389d3b0
Nate Tripp <nate.tripp13448@gmail.com>
2023-12-19 19:47:59 -0700

    Added Container

---
+++ TnTComponents/TnTContainer.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace TnTComponents;
+public partial class TnTContainer {
+    [Parameter]
+    public override string Class { get; set; } = "tnt-container";
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+}
