9746721b85f37c78f4deef9b8b056ab8a16cb67c
Nate Tripp <nate.tripp13448@gmail.com>
2023-12-26 12:19:36 -0700

    Added constraints to ripple

--- TnTComponents/TnTButton.razor.js
+++ TnTComponents/TnTButton.razor.js
@@ -1,10 +1,13 @@
 export function onUpdate() {
     const url = new URL(import.meta.url);
     const className = url.searchParams.get("class");
+    const elementConstraint = url.searchParams.get("elementConstraint");
     document.addEventListener('click', function (e) {
         const elem = e.target.closest(`.${className}`);
         if (elem) {
-            TnTComponents.ripple(elem, e);
+            if (!elementConstraint || elementConstraint.includes(elem.tagName.toLowerCase())) {
+                TnTComponents.ripple(elem, e);
+            }
         }
     });
 }
