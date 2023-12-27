b9f325b8eb46a4d9b0befc1139613b0e26d37ca7
Nate Tripp <nate.tripp13448@gmail.com>
2023-12-27 10:18:43 -0700

    Updated to allow form fields to be used with form

--- TnTComponents/Forms/TnTInputBase.razor.cs
+++ TnTComponents/Forms/TnTInputBase.razor.cs
@@ -113,56 +113,59 @@ public abstract partial class TnTInputBase<TInputType> {
     protected virtual string GetFormType() => FormType.ToString().ToLower();
 
     protected override void OnParametersSet() {
-        var modelType = EditContext.Model.GetType();
         var fieldName = FieldIdentifier.FieldName;
 
-        var property = modelType.GetProperty(fieldName) ?? throw new InvalidOperationException($"Unable to bind {fieldName}. It is not a valid property of {modelType}!");
+        if (EditContext is not null) {
 
-        var maxLengthAttribute = property.GetCustomAttribute<MaxLengthAttribute>();
-        var minLengthAttribute = property.GetCustomAttribute<MinLengthAttribute>();
-        var stringLengthAttribute = property.GetCustomAttribute<StringLengthAttribute>();
-        var rangeAttribute = property.GetCustomAttribute<RangeAttribute>();
-        var requiredAttribute = property.GetCustomAttribute<RequiredAttribute>();
-        var regularExpressionAttribute = property.GetCustomAttribute<RegularExpressionAttribute>();
+            var modelType = EditContext.Model.GetType();
+            var property = modelType.GetProperty(fieldName) ?? throw new InvalidOperationException($"Unable to bind {fieldName}. It is not a valid property of {modelType}!");
 
-        if (maxLengthAttribute is not null) {
-            MaxLength = maxLengthAttribute.Length;
-        }
+            var maxLengthAttribute = property.GetCustomAttribute<MaxLengthAttribute>();
+            var minLengthAttribute = property.GetCustomAttribute<MinLengthAttribute>();
+            var stringLengthAttribute = property.GetCustomAttribute<StringLengthAttribute>();
+            var rangeAttribute = property.GetCustomAttribute<RangeAttribute>();
+            var requiredAttribute = property.GetCustomAttribute<RequiredAttribute>();
+            var regularExpressionAttribute = property.GetCustomAttribute<RegularExpressionAttribute>();
 
-        if (minLengthAttribute is not null) {
-            MinLength = minLengthAttribute.Length;
-        }
+            if (maxLengthAttribute is not null) {
+                MaxLength = maxLengthAttribute.Length;
+            }
 
-        if (property.PropertyType == typeof(string)) {
-            if (stringLengthAttribute is not null) {
-                if (stringLengthAttribute.MinimumLength != 0) {
-                    if (minLengthAttribute is not null) {
-                        _logger?.LogWarning($"Overriding {nameof(MinLengthAttribute)}.{nameof(MinLengthAttribute.Length)} value with {nameof(StringLengthAttribute.MinimumLength)} since it was also set.");
+            if (minLengthAttribute is not null) {
+                MinLength = minLengthAttribute.Length;
+            }
+
+            if (property.PropertyType == typeof(string)) {
+                if (stringLengthAttribute is not null) {
+                    if (stringLengthAttribute.MinimumLength != 0) {
+                        if (minLengthAttribute is not null) {
+                            _logger?.LogWarning($"Overriding {nameof(MinLengthAttribute)}.{nameof(MinLengthAttribute.Length)} value with {nameof(StringLengthAttribute.MinimumLength)} since it was also set.");
+                        }
+
+                        MinLength = stringLengthAttribute.MinimumLength;
                     }
-
-                    MinLength = stringLengthAttribute.MinimumLength;
+                    if (minLengthAttribute is not null) {
+                        _logger?.LogWarning($"Overriding {nameof(MaxLengthAttribute)}.{nameof(MaxLengthAttribute.Length)} value with {nameof(StringLengthAttribute.MaximumLength)} since it was also set.");
+                    }
+                    MaxLength = stringLengthAttribute.MaximumLength;
                 }
-                if (minLengthAttribute is not null) {
-                    _logger?.LogWarning($"Overriding {nameof(MaxLengthAttribute)}.{nameof(MaxLengthAttribute.Length)} value with {nameof(StringLengthAttribute.MaximumLength)} since it was also set.");
+            }
+
+            if (rangeAttribute is not null) {
+                if (rangeAttribute.OperandType != typeof(TInputType) && rangeAttribute.OperandType != typeof(TInputType?)) {
+                    throw new InvalidOperationException($"{rangeAttribute.OperandType.GetType().Name} must be the same type as {typeof(TInputType).Name}!");
                 }
-                MaxLength = stringLengthAttribute.MaximumLength;
+                MinValue = (TInputType?)rangeAttribute.Minimum;
+                MaxValue = (TInputType?)rangeAttribute.Maximum;
             }
-        }
 
-        if (rangeAttribute is not null) {
-            if (rangeAttribute.OperandType != typeof(TInputType) && rangeAttribute.OperandType != typeof(TInputType?)) {
-                throw new InvalidOperationException($"{rangeAttribute.OperandType.GetType().Name} must be the same type as {typeof(TInputType).Name}!");
+            if (requiredAttribute is not null) {
+                Required = true;
             }
-            MinValue = (TInputType?)rangeAttribute.Minimum;
-            MaxValue = (TInputType?)rangeAttribute.Maximum;
-        }
 
-        if (requiredAttribute is not null) {
-            Required = true;
-        }
-
-        if (regularExpressionAttribute is not null) {
-            Pattern = regularExpressionAttribute.Pattern;
+            if (regularExpressionAttribute is not null) {
+                Pattern = regularExpressionAttribute.Pattern;
+            }
         }
 
         AriaLabel ??= FieldIdentifier.FieldName;
@@ -170,7 +173,6 @@ public abstract partial class TnTInputBase<TInputType> {
         if (_parentFormType.HasValue) {
             FormType = _parentFormType.Value;
         }
-
         base.OnParametersSet();
     }
 }
