fa6ef1db5a7f333cd34a950d56a62bc97274c0ba
Nate Tripp <nate.tripp13448@gmail.com>
2024-01-29 16:25:29 -0700

    Added Divider

---
+++ TnTComponents/Core/TnTDivider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TnTComponents.Core;
+
+namespace TnTComponents;
+public class TnTDivider : ComponentBase {
+
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    [Parameter]
+    public TnTColor Color { get; set; } = TnTColor.OutlineVariant;
+
+    public string? Class => CssBuilder.Create()
+        .AddBackgroundColor(Color)
+        .Build();
+
+    [Parameter]
+    public string? Style { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder) {
+        builder.OpenElement(0, "hr");
+        builder.AddMultipleAttributes(10, AdditionalAttributes);
+        builder.AddAttribute(20, "class", Class);
+        builder.AddAttribute(30, "style", Style);
+        builder.CloseElement();
+    }
+}
+
