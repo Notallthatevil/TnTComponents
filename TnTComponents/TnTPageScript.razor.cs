9e9ea9cdf63fbe83225f12926b6d18f52799ccbb
Nate Tripp <nate.tripp13448@gmail.com>
2024-01-29 17:04:17 -0700

    Moved page script

--- TnTComponents/TnTPageScript.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-
-namespace TnTComponents;
-
-public partial class TnTPageScript {
-
-    [Parameter, EditorRequired]
-    public string Src { get; set; } = default!;
-}
