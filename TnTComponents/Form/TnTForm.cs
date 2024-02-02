bea8093a951bec886e787ac12cf3f21c98df2ec0
Nate Tripp <ntripp@workfirstcasualty.com>
2024-02-02 12:22:43 -0700

    Fixed form issues

--- TnTComponents/Form/TnTForm.cs
+++ TnTComponents/Form/TnTForm.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Rendering;
+using System.Linq.Expressions;
 using TnTComponents.Form;
 
 namespace TnTComponents;
--- TnTComponents/Form/TnTInputBase.cs
+++ TnTComponents/Form/TnTInputBase.cs
@@ -77,17 +77,6 @@ public abstract partial class TnTInputBase<TInputType> : InputBase<TInputType>, 
 
     public abstract InputType Type { get; }
 
-    protected string Name {
-        get {
-            if (AdditionalAttributes?.TryGetValue("name", out var name) == true) {
-                return Convert.ToString(name, CultureInfo.InvariantCulture) ?? string.Empty;
-            }
-            else {
-                return FieldIdentifier.FieldName;
-            }
-        }
-    }
-
     protected override void BuildRenderTree(RenderTreeBuilder builder) {
         builder.OpenElement(0, "span");
         builder.AddAttribute(10, "class", Class);
@@ -102,8 +91,8 @@ public abstract partial class TnTInputBase<TInputType> : InputBase<TInputType>, 
                 builder.OpenElement(100, "input");
                 builder.AddMultipleAttributes(110, AdditionalAttributes);
                 builder.AddAttribute(120, "type", Type.ToInputTypeString());
-                builder.AddAttribute(121, "name", Name);
-
+                builder.AddAttribute(121, "name", NameAttributeValue);
+                
                 if (CurrentValue is bool) {
                     builder.AddAttribute(125, "value", bool.TrueString);
                     builder.AddAttribute(126, "checked", BindConverter.FormatValue(CurrentValue));
