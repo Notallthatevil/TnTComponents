d15228736c70d5e1ae8f8841f5ebb3753a0ad35c
Nate Tripp <ntripp@workfirstcasualty.com>
2024-02-03 12:50:37 -0700

    Added basic input file.

---
+++ TnTComponents/Form/TnTInputFile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TnTComponents.Core;
+
+namespace TnTComponents.Form;
+public class TnTInputFile : InputFile {
+
+    public string FormCssClass => CssClassBuilder.Create()
+        .Build();
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder) {
+        builder.OpenElement(0, "span");
+        builder.AddAttribute(10, "class", FormCssClass);
+        {
+            builder.OpenRegion(20);
+            base.BuildRenderTree(builder);
+            builder.CloseRegion();
+        }
+
+        builder.CloseElement();
+    }
+}
+
