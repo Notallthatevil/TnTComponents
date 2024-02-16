b815dad7641319e64b590aeb5a0097aa96a03852
Nate Tripp <ntripp@workfirstcasualty.com>
2024-02-16 11:00:46 -0700

    Added render context

---
+++ TnTComponents/Core/TnTServerRenderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Core;
+/// <inheritdoc/>
+public sealed class TnTServerRenderContext(IHttpContextAccessor contextAccessor) : ITnTRenderContext {
+    /// <inheritdoc/>
+    public bool IsClient => false;
+
+    /// <inheritdoc/>
+    public bool IsServer => true;
+
+    /// <inheritdoc/>
+    public bool IsPrerendering => !contextAccessor.HttpContext?.Response.HasStarted ?? false;
+}
