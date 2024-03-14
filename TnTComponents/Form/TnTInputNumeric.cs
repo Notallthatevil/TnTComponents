3fa5ed2eb184617fd04e62a7296b93439f86291c
Nate Tripp <ntripp@workfirstcasualty.com>
2024-03-14 13:34:04 -0600

    Removed type constraint to allow use of nullable

--- TnTComponents/Form/TnTInputNumeric.cs
+++ TnTComponents/Form/TnTInputNumeric.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -9,22 +10,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TnTComponents;
-public class TnTInputNumeric<TNumericType> : TnTInputBase<TNumericType?> where TNumericType : INumberBase<TNumericType>? {
+public class TnTInputNumeric<TNumericType> : TnTInputBase<TNumericType> {
     public override InputType Type => InputType.Number;
 
-    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TNumericType? result, [NotNullWhen(false)] out string? validationErrorMessage) {
-        validationErrorMessage = null;
-        if (TNumericType.TryParse(value, CultureInfo.InvariantCulture, out result)) {
+
+    protected override void OnInitialized() {
+        base.OnInitialized();
+        Type type = Nullable.GetUnderlyingType(typeof(TNumericType)) ?? typeof(TNumericType);
+        if (type != typeof(int) && type != typeof(long) && type != typeof(short) && type != typeof(float) && type != typeof(double) && type != typeof(decimal)) {
+            throw new InvalidOperationException($"The type '{type}' is not a supported numeric type.");
+        }
+
+    }
+
+    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TNumericType result, [NotNullWhen(false)] out string? validationErrorMessage) {
+        if (BindConverter.TryConvertTo<TNumericType>(value, CultureInfo.InvariantCulture, out result)) {
+            validationErrorMessage = null;
             return true;
         }
-        else if (value is null) {
-            result = default;
-            return true;
-        }
-        else {
-            validationErrorMessage = $"Failed to parse {value} into a {typeof(TNumericType).Name}";
-            return false;
-        }
+
+        validationErrorMessage = string.Format(CultureInfo.InvariantCulture, "The {0} field must be a number.", base.DisplayName ?? base.FieldIdentifier.FieldName);
+        return false;
     }
 }
 
