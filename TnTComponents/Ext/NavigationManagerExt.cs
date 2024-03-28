681564e98ee56c477bf5f295ec5845eb0f518685
Nate Tripp <ntripp@workfirstcasualty.com>
2024-03-28 13:47:52 -0600

    Add NavigationManagerExt for query string manipulation
    This commit introduces a new static class `NavigationManagerExt` in the `TnTComponents.Ext` namespace. This class extends the `NavigationManager` class from the `Microsoft.AspNetCore.Components` namespace with two new methods: `UpdateOrReplaceParameters`. These methods allow for updating or replacing parameters in the query string of a URI managed by a `NavigationManager` instance. This enhancement provides more flexibility in handling URIs within the application.

---
+++ TnTComponents/Ext/NavigationManagerExt.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Ext;
+public static class NavigationManagerExt {
+
+
+    public static string UpdateOrReplaceParameters(this NavigationManager navManager, IReadOnlyDictionary<string, object?> parameters) {
+       return navManager.UpdateOrReplaceParameters(navManager.Uri, parameters);
+    }
+
+    public static string UpdateOrReplaceParameters(this NavigationManager navManager, string uri, IReadOnlyDictionary<string, object?> parameters) {
+        var uriBuilder = new UriBuilder(uri);
+        var currentParams = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+
+        var dictionary = new Dictionary<string, object?>(parameters);
+
+        foreach (var key in currentParams.AllKeys) {
+            if (!dictionary.ContainsKey(key)) {
+                dictionary[key] = currentParams[key];
+            }
+        }
+
+        return navManager.GetUriWithQueryParameters(uri, parameters);
+    }
+
+}
+
