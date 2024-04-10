2efa5d6f4d8573680ba8565d19d2124fd7c8852a
Nate Tripp <ntripp@workfirstcasualty.com>
2024-04-10 10:28:38 -0600

    Added a checkbox required attribute

---
+++ TnTComponents/Attributes/CheckboxRequiredAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Attributes;
+public class CheckboxRequiredAttribute : RangeAttribute{
+
+    public CheckboxRequiredAttribute(string errorMessage = "Required"): base(typeof(bool), minimum: "true", maximum: "true") {
+        ErrorMessage = errorMessage;
+    }
+
+}
+
