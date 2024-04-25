02ba3fc7ab130cb6044f92f8fbbda91ac26f6ef6
Nate Tripp <ntripp@workfirstcasualty.com>
2024-04-25 11:00:19 -0600

    `Updated TnTScheduler.razor and TnTScheduler.razor.cs with new properties and structure`
    ` `
    
    `Modified the TnTScheduler.razor file to include a new div element with additional attributes, CSS class, and CSS style. The CascadingValue component has been moved inside this div and the components for DisplayedView, FirstDate, and TasksList have been removed. In the TnTScheduler.razor.cs file, the TnTComponents.Core namespace has been added. New properties AdditionalAttributes, CssClass, CssStyle, and Element have been added to capture unmatched values, create CSS classes and styles, and hold a reference to the div element respectively. Also, several empty comment lines have been added as placeholders for future code or comments.`

--- TnTComponents/Scheduler/TnTScheduler.razor.cs
+++ TnTComponents/Scheduler/TnTScheduler.razor.cs
@@ -1,12 +1,47 @@
 ﻿using BlazorCalendar;
 using BlazorCalendar.Models;
 using Microsoft.AspNetCore.Components;
+using TnTComponents.Core;
 using TnTComponents.Scheduler;
 
 namespace TnTComponents;
 
 [CascadingTypeParameter(nameof(TEventType))]
 partial class TnTScheduler<TEventType> : CalendarBase where TEventType : TnTEvent {
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    public string? CssClass => CssClassBuilder.Create()
+        .AddFromAdditionalAttributes(AdditionalAttributes)
+        .AddClass("tnt-scheduler")
+        .Build();
+
+    public string? CssStyle => CssStyleBuilder.Create()
+        .AddFromAdditionalAttributes(AdditionalAttributes)
+        .Build();
+
+    public ElementReference Element { get; private set; }
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+
+
+
+
+
+
+
+
     [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
 
