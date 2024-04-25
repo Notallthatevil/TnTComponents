ce368d85d083783f691d9e539dfe4f0cb5e8fd4f
Nate Tripp <ntripp@workfirstcasualty.com>
2024-04-25 10:46:53 -0600

    Refactor Scheduler and replace CalendarContainer with TnTScheduler
    This commit includes a major refactoring of the `Scheduler.razor` file for improved readability, and the introduction of a new component `TnTScheduler` replacing the `CalendarContainer` component. The `CalendarContainer.razor` and `CalendarContainer.razor.cs` files have been removed, their functionality now handled by `TnTScheduler.razor` and `TnTScheduler.razor.cs`.
    
    A new abstract base class `ScheduleViewBase` has been defined in the added `ScheduleViewBase.cs` file, providing a foundation for scheduler views with various properties and an overridden `OnInitialized` method.
    
    A new class `TnTEvent` has been defined in the added `TnTEvent.cs` file, used as a type parameter in `TnTScheduler` and `ScheduleViewBase`.
    
    The `TnTScheduler` component, defined in the newly added `TnTScheduler.razor` and `TnTScheduler.razor.cs` files, inherits from the `CalendarBase` class and uses `TnTEvent` as a type parameter. It includes parameters for child content, first date, displayed view, and tasks list, with a cascading type parameter for the `TEventType` in the code-behind.

---
+++ TnTComponents/Scheduler/Infrastructure/ScheduleViewBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TnTComponents.Core;
+
+namespace TnTComponents.Scheduler.Infrastructure;
+public abstract class ScheduleViewBase<TEventType> : ComponentBase, ITnTComponentBase where TEventType : TnTEvent {
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+    [Parameter] public bool? AutoFocus { get; set; }
+
+    public string? CssClass => null;
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    public ElementReference Element { get; protected set; }
+
+    public string? Id { get; private set; }
+
+    public string? CssStyle => null;
+
+    protected override void OnInitialized() {
+        base.OnInitialized();
+        Id ??= TnTComponentIdentifier.NewId();
+    }
+}
+
---
+++ TnTComponents/Scheduler/TnTScheduler.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorCalendar;
+using BlazorCalendar.Models;
+using Microsoft.AspNetCore.Components;
+using TnTComponents.Scheduler;
+
+namespace TnTComponents;
+
+[CascadingTypeParameter(nameof(TEventType))]
+partial class TnTScheduler<TEventType> : CalendarBase where TEventType : TnTEvent {
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    [Parameter]
+    public DateTime FirstDate { get; set; }
+
+    [Parameter]
+    public DisplayedView DisplayedView { get; set; }
+
+    [Parameter]
+    public Tasks[]? TasksList { get; set; }
+}
