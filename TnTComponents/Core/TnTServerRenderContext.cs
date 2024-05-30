157e848944828abe20f30778fc84bfba24d2b8e2
Nate Tripp <ntripp@workfirstcasualty.com>
2024-05-30 10:30:54 -0600

    Attempting to remove HttpContextAccessor

--- TnTComponents/Core/TnTServerRenderContext.cs
+++ TnTComponents/Core/TnTServerRenderContext.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace TnTComponents.Core;
 
 /// <inheritdoc />
-public sealed class TnTServerRenderContext(IHttpContextAccessor contextAccessor) : ITnTRenderContext {
+public sealed class TnTServerRenderContext : ITnTRenderContext {
 
     /// <inheritdoc />
     public bool IsClient => false;
 
     /// <inheritdoc />
-    public bool IsPrerendering => !contextAccessor.HttpContext?.Response.HasStarted ?? false;
+    public bool IsPrerendering => !(_contextAccessor.HttpContext?.Response.HasStarted) ?? false;
 
     /// <inheritdoc />
     public bool IsServer => true;
+
+    private dynamic _contextAccessor;
+
+    public TnTServerRenderContext(IServiceProvider serviceProvider) {
+        const string HttpAccessor = "Microsoft.AspNetCore.Http.IHttpContextAccessor";
+        var type = Type.GetType("Microsoft.AspNetCore.Http.IHttpContextAccessor") ?? 
+            throw new InvalidOperationException($"Failed to locate type '{HttpAccessor}' in services. Did you forget to call builder.Services.AddHttpContextAccessor()?");
+        _contextAccessor = serviceProvider.GetRequiredService(type);
+    }
 }
