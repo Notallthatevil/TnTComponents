58a2a728aac9a075b40f39ab72899cb2dca33eaa
Nate Tripp <ntripp@workfirstcasualty.com>
2024-05-30 11:19:10 -0600

    Refactor TnTServerRenderContext to use IHttpContextAccessor
    Updated the `TnTServerRenderContext.cs` file to replace the `Microsoft.Extensions.DependencyInjection` namespace with `Microsoft.AspNetCore.Http`. The `TnTServerRenderContext` class now takes an `IHttpContextAccessor` as a constructor parameter, simplifying the code by directly injecting the required service. The `IsPrerendering` property now uses the newly injected `contextAccessor` directly. Removed the private field `_contextAccessor` and the previous constructor that used `IServiceProvider` to get the `IHttpContextAccessor` as they are no longer needed. Added a new package reference to `Microsoft.AspNetCore.Http.Abstractions` version `2.2.0` in the `TnTComponents.csproj` file, which provides the `IHttpContextAccessor` interface used in the updated `TnTServerRenderContext` class.

--- TnTComponents/Core/TnTServerRenderContext.cs
+++ TnTComponents/Core/TnTServerRenderContext.cs
@@ -1,25 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace TnTComponents.Core;
 
 /// <inheritdoc />
-public sealed class TnTServerRenderContext : ITnTRenderContext {
+public sealed class TnTServerRenderContext(IHttpContextAccessor contextAccessor) : ITnTRenderContext {
 
     /// <inheritdoc />
     public bool IsClient => false;
 
     /// <inheritdoc />
-    public bool IsPrerendering => !(_contextAccessor.HttpContext?.Response.HasStarted) ?? false;
+    public bool IsPrerendering => !contextAccessor.HttpContext?.Response.HasStarted ?? false;
 
     /// <inheritdoc />
     public bool IsServer => true;
-
-    private dynamic _contextAccessor;
-
-    public TnTServerRenderContext(IServiceProvider serviceProvider) {
-        const string HttpAccessor = "Microsoft.AspNetCore.Http.IHttpContextAccessor";
-        var type = Type.GetType("Microsoft.AspNetCore.Http.IHttpContextAccessor") ?? 
-            throw new InvalidOperationException($"Failed to locate type '{HttpAccessor}' in services. Did you forget to call builder.Services.AddHttpContextAccessor()?");
-        _contextAccessor = serviceProvider.GetRequiredService(type);
-    }
 }
