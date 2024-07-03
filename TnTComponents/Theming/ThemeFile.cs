a3f62276ca56d35bdffec83f5944d31602e19ccc
Nate Tripp <nate.tripp13448@gmail.com>
2024-07-03 10:43:46 -0600

    Refactor theme system and update TnTColor
    This commit introduces significant changes to the theming system and the TnTColor enum to enhance flexibility and maintainability. Key modifications include:
    
    - **TnTColor Enum Modifications:**
      - Added `SurfaceTint` and detailed color specifications for improved color management.
      - Reordered and removed several enum members to streamline the color palette.
      - Introduced new members like `Warning`, `OnWarning`, and their containers for better warning color handling.
    
    - **New ThemeFile Classes:**
      - Added `ThemeFile.cs` with classes (`ThemeFile`, `CoreColors`, `Scheme`, `ColorScheme`) designed to encapsulate theme-related information, facilitating easier theme management and customization.
    
    - **TnTThemeDesign Class Enhancements:**
      - Removed hardcoded color parameters in favor of dynamic theme loading from theme files, significantly simplifying theme customization.
      - Enhanced dynamic theme application, including support for light and dark modes, and streamlined CSS generation based on theme properties.
    
    Overall, these changes simplify the theme design process, allow for dynamic theme loading, and provide a more structured approach to theme and color management.

---
+++ TnTComponents/Theming/ThemeFile.cs
@@ -0,0 +1,87 @@
+﻿
+namespace TnTComponents.Theming;
+
+internal class ThemeFile {
+    public string? Description { get; set; } 
+    public string? Seed { get; set; } 
+    public CoreColors? CoreColors { get; set; } 
+    public Scheme? Schemes { get; set; } 
+}
+
+internal class CoreColors {
+    public string? Primary { get; set; } 
+    public string? Secondary { get; set; } 
+    public string? Tertiary { get; set; } 
+    public string? Neutral { get; set; } 
+    public string? NeutralVariant { get; set; } 
+}
+
+internal class Scheme {
+    public ColorScheme? Light { get; set; } 
+    public ColorScheme? Dark { get; set; } 
+}
+
+internal class ColorScheme {
+    public string? Primary { get; set; } 
+    public string? SurfaceTint { get; set; } 
+    public string? OnPrimary { get; set; } 
+    public string? PrimaryContainer { get; set; } 
+    public string? OnPrimaryContainer { get; set; } 
+    public string? Secondary { get; set; } 
+    public string? OnSecondary { get; set; } 
+    public string? SecondaryContainer { get; set; } 
+    public string? OnSecondaryContainer { get; set; } 
+    public string? Tertiary { get; set; } 
+    public string? OnTertiary { get; set; } 
+    public string? TertiaryContainer { get; set; } 
+    public string? OnTertiaryContainer { get; set; } 
+    public string? Error { get; set; } 
+    public string? OnError { get; set; } 
+    public string? ErrorContainer { get; set; } 
+    public string? OnErrorContainer { get; set; } 
+    public string? Background { get; set; } 
+    public string? OnBackground { get; set; } 
+    public string? Surface { get; set; } 
+    public string? OnSurface { get; set; } 
+    public string? SurfaceVariant { get; set; } 
+    public string? OnSurfaceVariant { get; set; } 
+    public string? Outline { get; set; } 
+    public string? OutlineVariant { get; set; } 
+    public string? Shadow { get; set; } 
+    public string? Scrim { get; set; } 
+    public string? InverseSurface { get; set; } 
+    public string? InverseOnSurface { get; set; } 
+    public string? InversePrimary { get; set; } 
+    public string? PrimaryFixed { get; set; } 
+    public string? OnPrimaryFixed { get; set; } 
+    public string? PrimaryFixedDim { get; set; } 
+    public string? OnPrimaryFixedVariant { get; set; } 
+    public string? SecondaryFixed { get; set; } 
+    public string? OnSecondaryFixed { get; set; } 
+    public string? SecondaryFixedDim { get; set; } 
+    public string? OnSecondaryFixedVariant { get; set; } 
+    public string? TertiaryFixed { get; set; } 
+    public string? OnTertiaryFixed { get; set; } 
+    public string? TertiaryFixedDim { get; set; } 
+    public string? OnTertiaryFixedVariant { get; set; } 
+    public string? SurfaceDim { get; set; } 
+    public string? SurfaceBright { get; set; } 
+    public string? SurfaceContainerLowest { get; set; } 
+    public string? SurfaceContainerLow { get; set; } 
+    public string? SurfaceContainer { get; set; } 
+    public string? SurfaceContainerHigh { get; set; } 
+    public string? SurfaceContainerHighest { get; set; } 
+    public string? Success { get; set; } 
+    public string? OnSuccess { get; set; } 
+    public string? SuccessContainer { get; set; } 
+    public string? OnSuccessContainer { get; set; } 
+    public string? Warning { get; set; } 
+    public string? OnWarning { get; set; } 
+    public string? WarningContainer { get; set; } 
+    public string? OnWarningContainer { get; set; } 
+    public string? Info { get; set; } 
+    public string? OnInfo { get; set; } 
+    public string? InfoContainer { get; set; } 
+    public string? OnInfoContainer { get; set; } 
+
+}
--- TnTComponents/Theming/TnTThemeDesign.cs
+++ TnTComponents/Theming/TnTThemeDesign.cs
@@ -1,34 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System.Drawing;
+using System.Reflection;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using TnTComponents.Ext;
+using TnTComponents.Theming;
 
 namespace TnTComponents;
 
 public class TnTThemeDesign : IComponent {
 
+    [Parameter, EditorRequired]
+    public string ThemeFileName { get; set; } = default!;
+
     [Parameter]
     public bool AllowColorModeToggle { get; set; } = true;
-
-    [Parameter]
-    public Color BackgroundDark { get; set; } = ColorTranslator.FromHtml("#1d1b1e");
-
-    [Parameter]
-    public Color BackgroundLight { get; set; } = ColorTranslator.FromHtml("#Fef7ff");
-
-    public Color Black => Color.Black;
-
-    [Parameter]
-    public Color ErrorContainerDark { get; set; } = ColorTranslator.FromHtml("#93000a");
-
-    [Parameter]
-    public Color ErrorContainerLight { get; set; } = ColorTranslator.FromHtml("#Ffdad6");
-
-    [Parameter]
-    public Color ErrorDark { get; set; } = ColorTranslator.FromHtml("#Ffb4ab");
-
-    [Parameter]
-    public Color ErrorLight { get; set; } = ColorTranslator.FromHtml("#Ba1a1a");
 
     [Parameter]
     public double FooterHeight { get; set; } = 4;
@@ -37,278 +25,16 @@ public class TnTThemeDesign : IComponent {
     public double HeaderHeight { get; set; } = 4;
 
     [Parameter]
-    public Color InfoContainerDark { get; set; } = ColorTranslator.FromHtml("#004883");
-
-    [Parameter]
-    public Color InfoContainerLight { get; set; } = ColorTranslator.FromHtml("#D3e3ff");
-
-    [Parameter]
-    public Color InfoDark { get; set; } = ColorTranslator.FromHtml("#A3c9ff");
-
-    [Parameter]
-    public Color InfoLight { get; set; } = ColorTranslator.FromHtml("#0d60a8");
-
-    [Parameter]
-    public Color InverseOnSurfaceDark { get; set; } = ColorTranslator.FromHtml("#1d1b1e");
-
-    [Parameter]
-    public Color InverseOnSurfaceLight { get; set; } = ColorTranslator.FromHtml("#F5eff4");
-
-    [Parameter]
-    public Color InversePrimaryDark { get; set; } = ColorTranslator.FromHtml("#7547ad");
-
-    [Parameter]
-    public Color InversePrimaryLight { get; set; } = ColorTranslator.FromHtml("#D9b9ff");
-
-    [Parameter]
-    public Color InverseSurfaceDark { get; set; } = ColorTranslator.FromHtml("#E7e1e5");
-
-    [Parameter]
-    public Color InverseSurfaceLight { get; set; } = ColorTranslator.FromHtml("#322f33");
-
-    [Parameter]
-    public Color OnBackgroundDark { get; set; } = ColorTranslator.FromHtml("#E7e1e5");
-
-    [Parameter]
-    public Color OnBackgroundLight { get; set; } = ColorTranslator.FromHtml("#1d1b20");
-
-    [Parameter]
-    public Color OnErrorContainerDark { get; set; } = ColorTranslator.FromHtml("#Ffdad6");
-
-    [Parameter]
-    public Color OnErrorContainerLight { get; set; } = ColorTranslator.FromHtml("#410002");
-
-    [Parameter]
-    public Color OnErrorDark { get; set; } = ColorTranslator.FromHtml("#690005");
-
-    [Parameter]
-    public Color OnErrorLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnInfoContainerDark { get; set; } = ColorTranslator.FromHtml("#D3e3ff");
-
-    [Parameter]
-    public Color OnInfoContainerLight { get; set; } = ColorTranslator.FromHtml("#001c39");
-
-    [Parameter]
-    public Color OnInfoDark { get; set; } = ColorTranslator.FromHtml("#00315c");
-
-    [Parameter]
-    public Color OnInfoLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnPrimaryContainerDark { get; set; } = ColorTranslator.FromHtml("#Eedbff");
-
-    [Parameter]
-    public Color OnPrimaryContainerLight { get; set; } = ColorTranslator.FromHtml("#2a0054");
-
-    [Parameter]
-    public Color OnPrimaryDark { get; set; } = ColorTranslator.FromHtml("#440e7c");
-
-    [Parameter]
-    public Color OnPrimaryLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnSecondaryContainerDark { get; set; } = ColorTranslator.FromHtml("#Ffdf91");
-
-    [Parameter]
-    public Color OnSecondaryContainerLight { get; set; } = ColorTranslator.FromHtml("#241a00");
-
-    [Parameter]
-    public Color OnSecondaryDark { get; set; } = ColorTranslator.FromHtml("#3d2e00");
-
-    [Parameter]
-    public Color OnSecondaryLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnSuccessContainerDark { get; set; } = ColorTranslator.FromHtml("#54febf");
-
-    [Parameter]
-    public Color OnSuccessContainerLight { get; set; } = ColorTranslator.FromHtml("#002115");
-
-    [Parameter]
-    public Color OnSuccessDark { get; set; } = ColorTranslator.FromHtml("#003826");
-
-    [Parameter]
-    public Color OnSuccessLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnSurfaceDark { get; set; } = ColorTranslator.FromHtml("#E7e1e5");
-
-    [Parameter]
-    public Color OnSurfaceLight { get; set; } = ColorTranslator.FromHtml("#1d1b20");
-
-    [Parameter]
-    public Color OnSurfaceVariantDark { get; set; } = ColorTranslator.FromHtml("#Ccc4cf");
-
-    [Parameter]
-    public Color OnSurfaceVariantLight { get; set; } = ColorTranslator.FromHtml("#49454f");
-
-    [Parameter]
-    public Color OnTertiaryContainerDark { get; set; } = ColorTranslator.FromHtml("#D8e2ff");
-
-    [Parameter]
-    public Color OnTertiaryContainerLight { get; set; } = ColorTranslator.FromHtml("#001a41");
-
-    [Parameter]
-    public Color OnTertiaryDark { get; set; } = ColorTranslator.FromHtml("#002e69");
-
-    [Parameter]
-    public Color OnTertiaryLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OnWarningContainerDark { get; set; } = ColorTranslator.FromHtml("#Ffddb6");
-
-    [Parameter]
-    public Color OnWarningContainerLight { get; set; } = ColorTranslator.FromHtml("#2a1800");
-
-    [Parameter]
-    public Color OnWarningDark { get; set; } = ColorTranslator.FromHtml("#462a00");
-
-    [Parameter]
-    public Color OnWarningLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color OutlineDark { get; set; } = ColorTranslator.FromHtml("#958e99");
-
-    [Parameter]
-    public Color OutlineLight { get; set; } = ColorTranslator.FromHtml("#7b757f");
-
-    [Parameter]
-    public Color OutlineVariantDark { get; set; } = ColorTranslator.FromHtml("#4a454e");
-
-    [Parameter]
-    public Color OutlineVariantLight { get; set; } = ColorTranslator.FromHtml("#Ccc4cf");
-
-    [Parameter]
-    public Color PrimaryContainerDark { get; set; } = ColorTranslator.FromHtml("#5c2d94");
-
-    [Parameter]
-    public Color PrimaryContainerLight { get; set; } = ColorTranslator.FromHtml("#Eedbff");
-
-    [Parameter]
-    public Color PrimaryDark { get; set; } = ColorTranslator.FromHtml("#D9b9ff");
-
-    [Parameter]
-    public Color PrimaryLight { get; set; } = ColorTranslator.FromHtml("#7547ad");
-
-    [Parameter]
-    public Color ScrimDark { get; set; } = ColorTranslator.FromHtml("#000000");
-
-    [Parameter]
-    public Color ScrimLight { get; set; } = ColorTranslator.FromHtml("#000000");
-
-    [Parameter]
-    public Color SecondaryContainerDark { get; set; } = ColorTranslator.FromHtml("#584400");
-
-    [Parameter]
-    public Color SecondaryContainerLight { get; set; } = ColorTranslator.FromHtml("#Ffdf91");
-
-    [Parameter]
-    public Color SecondaryDark { get; set; } = ColorTranslator.FromHtml("#F3c000");
-
-    [Parameter]
-    public Color SecondaryLight { get; set; } = ColorTranslator.FromHtml("#755b00");
-
-    [Parameter]
-    public Color ShadowDark { get; set; } = ColorTranslator.FromHtml("#Ababab");
-
-    [Parameter]
-    public Color ShadowLight { get; set; } = ColorTranslator.FromHtml("#565656");
-
-    [Parameter]
     public double SideNavWidth { get; set; } = 16;
-
-    [Parameter]
-    public Color SuccessContainerDark { get; set; } = ColorTranslator.FromHtml("#005139");
-
-    [Parameter]
-    public Color SuccessContainerLight { get; set; } = ColorTranslator.FromHtml("#54febf");
-
-    [Parameter]
-    public Color SuccessDark { get; set; } = ColorTranslator.FromHtml("#28e0a4");
-
-    [Parameter]
-    public Color SuccessLight { get; set; } = ColorTranslator.FromHtml("#006c4c");
-
-    [Parameter]
-    public Color SurfaceContainerDark { get; set; } = ColorTranslator.FromHtml("#211f26");
-
-    [Parameter]
-    public Color SurfaceContainerHighDark { get; set; } = ColorTranslator.FromHtml("#2b2930");
-
-    [Parameter]
-    public Color SurfaceContainerHighestDark { get; set; } = ColorTranslator.FromHtml("#36343b");
-
-    [Parameter]
-    public Color SurfaceContainerHighestLight { get; set; } = ColorTranslator.FromHtml("#E7e1e5");
-
-    [Parameter]
-    public Color SurfaceContainerHighLight { get; set; } = ColorTranslator.FromHtml("#Ece6f0");
-
-    [Parameter]
-    public Color SurfaceContainerLight { get; set; } = ColorTranslator.FromHtml("#F3edf7");
-
-    [Parameter]
-    public Color SurfaceContainerLowDark { get; set; } = ColorTranslator.FromHtml("#1d1b1e");
-
-    [Parameter]
-    public Color SurfaceContainerLowestDark { get; set; } = ColorTranslator.FromHtml("#0f0d13");
-
-    [Parameter]
-    public Color SurfaceContainerLowestLight { get; set; } = ColorTranslator.FromHtml("#Ffffff");
-
-    [Parameter]
-    public Color SurfaceContainerLowLight { get; set; } = ColorTranslator.FromHtml("#F7f2fa");
-
-    [Parameter]
-    public Color SurfaceDark { get; set; } = ColorTranslator.FromHtml("#1d1b1e");
-
-    [Parameter]
-    public Color SurfaceLight { get; set; } = ColorTranslator.FromHtml("#Fef7ff");
-
-    [Parameter]
-    public Color SurfaceTintDark { get; set; } = ColorTranslator.FromHtml("#D9b9ff");
-
-    [Parameter]
-    public Color SurfaceTintLight { get; set; } = ColorTranslator.FromHtml("#7547ad");
-
-    [Parameter]
-    public Color SurfaceVariantDark { get; set; } = ColorTranslator.FromHtml("#4a454e");
-
-    [Parameter]
-    public Color SurfaceVariantLight { get; set; } = ColorTranslator.FromHtml("#E8e0eb");
-
-    [Parameter]
-    public Color TertiaryContainerDark { get; set; } = ColorTranslator.FromHtml("#004494");
-
-    [Parameter]
-    public Color TertiaryContainerLight { get; set; } = ColorTranslator.FromHtml("#D8e2ff");
-
-    [Parameter]
-    public Color TertiaryDark { get; set; } = ColorTranslator.FromHtml("#Adc6ff");
-
-    [Parameter]
-    public Color TertiaryLight { get; set; } = ColorTranslator.FromHtml("#005ac1");
 
     [Parameter]
     public Theme Theme { get; set; }
 
     public Color Transparent => Color.Transparent;
 
-    [Parameter]
-    public Color WarningContainerDark { get; set; } = ColorTranslator.FromHtml("#643f00");
-
-    [Parameter]
-    public Color WarningContainerLight { get; set; } = ColorTranslator.FromHtml("#Ffddb6");
-
-    [Parameter]
-    public Color WarningDark { get; set; } = ColorTranslator.FromHtml("#Ffb95a");
-
-    [Parameter]
-    public Color WarningLight { get; set; } = ColorTranslator.FromHtml("#845400");
-
     public Color White => Color.White;
+    public Color Black => Color.Black;
+    private ThemeFile? _themeFile;
 
     private RenderHandle _renderHandle;
 
@@ -316,26 +42,52 @@ public class TnTThemeDesign : IComponent {
         _renderHandle = renderHandle;
     }
 
-    public Task SetParametersAsync(ParameterView parameters) {
-        var allProperties = GetType().GetProperties().ToDictionary(kv => kv.Name, p => p);
+    public async Task SetParametersAsync(ParameterView parameters) {
         foreach (var entry in parameters) {
-            if (allProperties.TryGetValue(entry.Name, out var prop)) {
-                prop.SetValue(this, entry.Value);
+            GetType().GetProperties().FirstOrDefault(p => p.Name == entry.Name)?.SetValue(this, entry.Value);
+        }
+
+        var allProperties = GetType().GetProperties().Select(p => new KeyValuePair<string, object?>(p.Name, p.GetValue(this)));
+        var themeFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", ThemeFileName);
+
+        if (File.Exists(themeFile)) {
+            await using var fs = File.OpenRead(themeFile);
+            _themeFile = await JsonSerializer.DeserializeAsync<ThemeFile>(fs, new JsonSerializerOptions() {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+
+        IEnumerable<KeyValuePair<string, object?>> properties = [];
+
+        const string themeColor = "ThemeColor";
+        if (_themeFile is not null) {
+            if (_themeFile.Schemes is not null) {
+                if (_themeFile.Schemes.Light is not null) {
+                    properties = properties.Concat(_themeFile.Schemes.Light.GetType().GetProperties().Select(p => new KeyValuePair<string, object?>($"{themeColor}{p.Name}Light", p.GetValue(_themeFile.Schemes.Light))));
+                }
+
+                if (_themeFile.Schemes.Dark is not null) {
+                    properties = properties.Concat(_themeFile.Schemes.Dark.GetType().GetProperties().Select(p => new KeyValuePair<string, object?>($"{themeColor}{p.Name}Dark", p.GetValue(_themeFile.Schemes.Dark))));
+                }
             }
         }
+
+        allProperties = allProperties.Concat(properties)
+            .Where(p => p.Value is not null)
+            .Where(p => p.Key != nameof(ThemeFileName));
 
         var other = new StringBuilder(":root{");
         var darkTheme = new StringBuilder(":root{");
         var lightTheme = new StringBuilder(":root{");
 
-        foreach (var prop in allProperties.Select(kv => kv.Value)) {
-            var propName = prop.Name.SplitPascalCase("-").ToLower();
-            var value = prop.GetValue(this);
-
-            if (value is Color color) {
-                var darkModeColor = propName.Contains("dark");
-                propName = propName.Replace("-dark", string.Empty);
-                propName = propName.Replace("-light", string.Empty);
+        foreach (var (name, value) in allProperties) {
+            if (name.StartsWith(themeColor, StringComparison.OrdinalIgnoreCase)) {
+                var darkModeColor = name.EndsWith("dark", StringComparison.OrdinalIgnoreCase);
+                var propName = name.Replace($"{themeColor}", string.Empty);
+                propName = propName.Replace("Dark", string.Empty, StringComparison.OrdinalIgnoreCase);
+                propName = propName.Replace("Light", string.Empty, StringComparison.OrdinalIgnoreCase);
+                propName = propName.SplitPascalCase("-").ToLower();
+                var color = ColorTranslator.FromHtml(value as string ?? $"000000");
                 if (darkModeColor) {
                     darkTheme.Append("--tnt-color-").Append(propName).Append(':').Append($"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}").Append(';');
                 }
@@ -343,10 +95,17 @@ public class TnTThemeDesign : IComponent {
                     lightTheme.Append("--tnt-color-").Append(propName).Append(':').Append($"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}").Append(';');
                 }
             }
+            else if (value is Color color) {
+                var propName = name.SplitPascalCase("-").ToLower();
+                darkTheme.Append("--tnt-color-").Append(propName).Append(':').Append($"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}").Append(';');
+                lightTheme.Append("--tnt-color-").Append(propName).Append(':').Append($"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}").Append(';');
+            }
             else if (value is double d) {
+                var propName = name.SplitPascalCase("-").ToLower();
                 other.Append("--tnt-").Append(propName).Append(':').Append(d).Append("rem;");
             }
             else {
+                var propName = name.SplitPascalCase("-").ToLower();
                 other.Append("--tnt-").Append(propName).Append(":'").Append(value?.ToString()?.ToLower()).Append("';");
             }
         }
@@ -361,34 +120,15 @@ public class TnTThemeDesign : IComponent {
             builder.CloseElement();
 
             builder.OpenElement(2, "style");
-            if (Theme == Theme.System) {
-                //builder.AddAttribute(3, "media", "(prefers-color-scheme:dark)");
-            }
-            else if (Theme == Theme.Dark) {
-                //builder.AddAttribute(3, "media", "all");
-            }
-            else {
-                //builder.AddAttribute(3, "media", "not all");
-            }
             builder.AddAttribute(4, "id", "tnt-theme-design-dark");
             builder.AddContent(5, darkTheme.ToString());
             builder.CloseElement();
 
             builder.OpenElement(6, "style");
-            if (Theme == Theme.System) {
-                //builder.AddAttribute(7, "media", "(prefers-color-scheme:light)");
-            }
-            else if (Theme == Theme.Dark) {
-                //builder.AddAttribute(7, "media", "not all");
-            }
-            else {
-                //builder.AddAttribute(7, "media", "all");
-            }
             builder.AddAttribute(8, "id", "tnt-theme-design-light");
             builder.AddContent(9, lightTheme.ToString());
             builder.CloseElement();
         }));
 
-        return Task.CompletedTask;
     }
 }
