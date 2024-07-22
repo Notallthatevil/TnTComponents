b83a1bc423790829b82fd9791bdcafcb360b17a9
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-07-22 09:21:03 -0600

    Add ITnTInteractable and ITnTStyleable interfaces
    Added two new interfaces to the TnTComponents.Interfaces namespace: ITnTInteractable, which includes a Disabled property to indicate if a component is disabled, and ITnTStyleable, a placeholder for future styling capabilities.

---
+++ TnTComponents/Interfaces/ITnTInteractable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Interfaces;
+public interface ITnTInteractable {
+    /// <summary>
+    /// Gets or sets a value indicating whether the component is disabled.
+    /// </summary>
+    bool Disabled { get; set; }
+}
+
