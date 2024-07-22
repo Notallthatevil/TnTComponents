ad86db8ee93c223f051718795c3fbbc09daacad1
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-07-22 09:58:49 -0600

    Enhance ITnTInteractable interface docs and props
    - Improved documentation for the ITnTInteractable interface to clarify its purpose and properties.
    - Modified the `Disabled` property to be read-only, preventing external modifications.
    - Added a new nullable string property `Name` to provide the name of the component, which is read-only.

--- TnTComponents/Interfaces/ITnTInteractable.cs
+++ TnTComponents/Interfaces/ITnTInteractable.cs
@@ -5,10 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TnTComponents.Interfaces;
+/// <summary>
+/// Represents an interactable component.
+/// </summary>
 public interface ITnTInteractable {
     /// <summary>
     /// Gets or sets a value indicating whether the component is disabled.
     /// </summary>
-    bool Disabled { get; set; }
+    bool Disabled { get; }
+
+    /// <summary>
+    /// Gets the name of the component.
+    /// </summary>
+    string? Name { get; }
 }
 
