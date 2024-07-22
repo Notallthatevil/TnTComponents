719da15c3748feca7f6c20a89889586aca5f3d8d
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-07-22 09:59:32 -0600

    Add using directives and new interface property
    Added `using Microsoft.AspNetCore.Components;` directive and introduced a new nullable property `TextAlign? TextAlign { get; set; }` to the `ITnTStyleable` interface for enhanced styling capabilities.

--- TnTComponents/Interfaces/ITnTStyleable.cs
+++ TnTComponents/Interfaces/ITnTStyleable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,6 @@ using System.Threading.Tasks;
 
 namespace TnTComponents.Interfaces;
 public interface ITnTStyleable {
+    TextAlign? TextAlign { get; set; }
 }
 
