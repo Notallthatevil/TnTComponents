33837e0ae1e86e0186d7af9aef29321479d9e741
Nate Tripp <nate.tripp13448@gmail.com>
2024-07-30 13:42:12 -0600

    Refactor Typeahead component and update styles
    Refactored `TnTTypeahead` component:
    - Replaced `ItemSelected` with `ItemSelectedCallback`.
    - Updated parameters and methods for search and item selection.
    - Added necessary `@using` and `using` directives.
    
    Updated `Typeahead.razor` to use `ItemSelectedCallback`.
    
    Added "Typeahead" navigation link in `MainLayout.razor`.
    
    Refactored `TnTTypeahead.razor` and `TnTTypeahead.razor.cs`:
    - Removed original content.
    - Added new content with `@inherits TnTInteractableComponentBase`.
    
    Updated `_typeahead.scss`:
    - Added styles for `.tnt-typeahead-box` and `.tnt-typeahead-content`.
    - Adjusted progress indicator positioning and appearance.

---
+++ TnTComponents/Typeahead/TnTTypeahead.razor.cs
@@ -0,0 +1,144 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TnTComponents.Core;
+
+namespace TnTComponents;
+
+public partial class TnTTypeahead<TItem> {
+
+    public override string? ElementClass => CssClassBuilder.Create()
+        .AddFromAdditionalAttributes(AdditionalAttributes)
+        .AddClass("tnt-typeahead")
+        .Build();
+
+    public override string? ElementStyle => CssStyleBuilder.Create()
+        .AddFromAdditionalAttributes(AdditionalAttributes)
+        .Build();
+
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    [Parameter]
+    public string? Label { get; set; }
+
+    [Parameter]
+    public TnTColor ProgressColor { get; set; } = TnTColor.Primary;
+
+    [Parameter]
+    public FormAppearance Appearance { get; set; }
+
+    [Parameter]
+    public RenderFragment<TItem>? ResultTemplate { get; set; }
+
+    [Parameter]
+    public EventCallback<TItem> ItemSelectedCallback { get; set; }
+    [Parameter, EditorRequired]
+    public Func<string?, CancellationToken, Task<IEnumerable<TItem>>> ItemsLookupFunc { get; set; } = default!;
+
+    private TnTDebouncer _debouncer = new();
+
+    [Parameter]
+    public int DebounceMilliseconds { get; set; } = 300;
+
+    private string? _searchText;
+    private bool _searching;
+
+    private IEnumerable<TItem> _items = [];
+    private async Task SearchAsync(string? searchValue) {
+        _searching = true;
+        _items = [];
+        await _debouncer.DebounceAsync(DebounceMilliseconds, async token => {
+            var result = await ItemsLookupFunc.Invoke(searchValue, token);
+            _items = result;
+            _searching = false;
+        });
+    }
+
+    private async Task ItemSelectedAsync(TItem item) {
+        _searchText = null;
+        _items = [];
+        _searching = false;
+        await ItemSelectedCallback.InvokeAsync(item);
+    }
+
+    //[Parameter]
+    //public int DebounceMilliseconds { get; set; } = 300;
+
+
+    //[Parameter, EditorRequired]
+    //public EventCallback<TItem> ItemSelected { get; set; }
+
+    //[Parameter, EditorRequired]
+    //public Func<string?, CancellationToken, Task<IEnumerable<TItem>>> ItemsLookupFunc { get; set; } = default!;
+
+    //[Parameter]
+    //public EventCallback ItemsSetCallback { get; set; }
+
+    //[Parameter]
+    //public TnTColor? LabelBackgroundColor { get; set; }
+
+    //[Parameter]
+    //public TnTColor? ResultsViewBackgroundColor { get; set; } = TnTColor.Surface;
+
+    //[Parameter]
+    //public TnTBorderRadius? ResultsViewBorderRadius { get; set; } = new(2);
+
+    //[Parameter]
+    //public int ResultsViewElevation { get; set; } = 2;
+
+    //[Parameter]
+    //public TnTColor? ResultsViewTextColor { get; set; } = TnTColor.OnSurface;
+
+    //[Parameter]
+    //public RenderFragment<TItem>? ResultTemplate { get; set; }
+
+    //[Parameter]
+    //public TnTColor SearchProgressColor { get; set; } = TnTColor.Secondary;
+
+    //[Parameter]
+    //public TnTIcon StartIcon { get; set; } = new MaterialIcon(MaterialIcon.Search);
+
+    //private TnTDebouncer _debouncer = new();
+    //private TnTInputText? _inputTextBox;
+    //private IEnumerable<TItem> _items = [];
+
+    //private bool _searching;
+    //private string? _searchText;
+
+    //private async Task ItemSelectedAsync(TItem item) {
+    //    _searchText = "";
+    //    _items = [];
+    //    _searching = false;
+    //    if (_inputTextBox is not null) {
+    //        await _inputTextBox.Element.FocusAsync();
+    //    }
+    //    await ItemSelected.InvokeAsync(item);
+    //}
+
+    //private async Task KeyPressedAsync(KeyboardEventArgs args) {
+    //    if (args.Key == "Escape") {
+    //        _items = [];
+    //        _searching = false;
+    //    }
+    //    else if (args.Key == "Enter" && _items.Any()) {
+    //        await ItemSelectedAsync(_items.First());
+    //    }
+    //}
+
+    //private async Task SearchAsync(string value) {
+    //    if (!string.IsNullOrWhiteSpace(value)) {
+    //        _searching = true;
+    //        await _debouncer.DebounceAsync(DebounceMilliseconds, async token => {
+    //            _items = await ItemsLookupFunc(value, token);
+    //            _searching = false;
+    //        });
+    //    }
+    //    else {
+    //        _searching = false;
+    //        await _debouncer.CancelAsync();
+    //        _items = [];
+    //    }
+    //    StateHasChanged();
+    //    await ItemsSetCallback.InvokeAsync();
+    //}
+}
