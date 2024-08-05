26a039557ffffa2b9a8b4616583809e163f9773d
Nate Tripp <nate.tripp13448@gmail.com>
2024-08-05 14:03:49 -0600

    Refactor TnTInputFile and remove IFormField interface
    The `IFormField` interface has been removed from the `TnTComponents.Form` namespace. This interface previously included properties for `EndIcon`, `Placeholder`, `StartIcon`, and `Type`.
    
    The `TnTInputFile` component has been significantly refactored:
    - It now inherits directly from `InputFile`.
    - Several new parameters have been added, including `Appearance`, `AutoFocus`, `Disabled`, `ElementId`, `ElementLang`, `ElementTitle`, `MaximumFileCount`, `OnInputFileChange`, and `ReadOnly`.
    - The `ElementClass` and `ElementStyle` properties have been introduced to build CSS classes and styles dynamically.
    - The `FieldDisabled` and `FieldReadonly` properties determine the disabled and readonly states based on the cascading `ITnTForm` parameter.
    - The `OnParametersSet` method has been overridden to update additional attributes.
    - The `BuildRenderTree` method has been overridden to customize the rendering of the component.
    - The `OnUploadFilesHandlerAsync` method handles file upload events and enforces the `MaximumFileCount`.
    
    The `TnTInputFile.razor` and `TnTInputFile.razor.cs` files have been removed, and their functionality has been integrated into the `TnTInputFile` class.
    
    In the `_buttons.scss` file, a new CSS rule has been added to style the file selector button within the `tnt-input-file` component. This rule extends the existing `tnt-label-large` class and applies various styles such as `appearance`, `user-select`, `min-height`, and `min-width`.

--- TnTComponents/Form/IFormField.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TnTComponents.Form;
-
-public interface IFormField : IFormItem {
-    TnTIcon? EndIcon { get; }
-    string? Placeholder { get; }
-    TnTIcon? StartIcon { get; }
-    InputType Type { get; }
-}
