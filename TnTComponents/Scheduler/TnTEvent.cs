6ba669518d5cb5ba1776e357e72fc987941592fa
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-11-26 14:20:53 -0700

    Add styles and form structure to TimeCard components
    Updated TimeCardDetailsCard.razor.scss to style .time-card-details-card, .table-container, and .add-button. Enhanced TimeCardForm.razor with TnTComponents.Scheduler, new form structure, and TnTScheduler component. Updated TimeCardForm.razor.cs with necessary using directives, parameters, properties, and methods. Added styles to TimeCardForm.razor.scss for .time-card-form and .time-card-top-bar.

--- TnTComponents/Scheduler/TnTEvent.cs
+++ TnTComponents/Scheduler/TnTEvent.cs
@@ -9,12 +9,11 @@ public record TnTEvent {
     public string? Description { get; set; }
     public DateTimeOffset EventStart { get; set; }
     public DateTimeOffset EventEnd { get; set; }
-
-    public TimeOnly StartTime => TimeOnly.FromTimeSpan(EventStart.LocalDateTime.TimeOfDay);
-    public TimeOnly EndTime => TimeOnly.FromTimeSpan(EventEnd.LocalDateTime.TimeOfDay);
-    public DateOnly StartDate => DateOnly.FromDateTime(EventStart.LocalDateTime.Date);
-    public DateOnly EndDate => DateOnly.FromDateTime(EventEnd.LocalDateTime.Date);
-    public TimeSpan Duration => EventEnd - EventStart;
+    public virtual TimeOnly StartTime => TimeOnly.FromTimeSpan(EventStart.LocalDateTime.TimeOfDay);
+    public virtual TimeOnly EndTime => TimeOnly.FromTimeSpan(EventEnd.LocalDateTime.TimeOfDay);
+    public virtual DateOnly StartDate => DateOnly.FromDateTime(EventStart.LocalDateTime.Date);
+    public virtual DateOnly EndDate => DateOnly.FromDateTime(EventEnd.LocalDateTime.Date);
+    public virtual TimeSpan Duration => EventEnd - EventStart;
 
     public TnTColor BackgroundColor { get; set; } = TnTColor.Tertiary;
     public TnTColor ForegroundColor { get; set; } = TnTColor.OnTertiary;
