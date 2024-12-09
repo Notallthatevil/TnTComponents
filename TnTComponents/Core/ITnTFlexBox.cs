a1b0faacd665716c54e95429fcd8901893c22f0a
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-12-09 14:40:17 -0700

    Refactor ITnTFlexBox namespace and add XML docs
    Changed the namespace of ITnTFlexBox from TnTComponents.Core to TnTComponents.Interfaces for better organization. Added XML documentation comments to the interface and its properties, including summaries for AlignContent, AlignItems, Direction, and JustifyContent, to improve code readability and provide context for developers.

--- TnTComponents/Core/ITnTFlexBox.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TnTComponents.Core;
-
-public interface ITnTFlexBox {
-    AlignContent? AlignContent { get; set; }
-    AlignItems? AlignItems { get; set; }
-    LayoutDirection? Direction { get; set; }
-    JustifyContent? JustifyContent { get; set; }
-}
