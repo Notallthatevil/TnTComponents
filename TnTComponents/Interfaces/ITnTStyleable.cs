70cd8d91bdb83b12c7c3314ccf1bec2e71827e6a
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-12-09 14:04:01 -0700

    Refactor interfaces and improve XML documentation
    - Move ITnTPageScriptComponent to TnTComponents.Interfaces and extend ITnTComponentBase
    - Reformat XML docs for ITnTComponentBase, ITnTInteractable, and ITnTStyleable
    - Modify ITnTInteractable to extend ITnTComponentBase and reorder properties
    - Reorder properties in ITnTStyleable

--- TnTComponents/Interfaces/ITnTStyleable.cs
+++ TnTComponents/Interfaces/ITnTStyleable.cs
@@ -7,11 +7,34 @@ using System.Threading.Tasks;
 using TnTComponents.Core;
 
 namespace TnTComponents.Interfaces;
-public interface ITnTStyleable {
-    TextAlign? TextAlignment { get; }
-    TnTColor BackgroundColor { get; }
-    TnTColor TextColor { get; }
-    int Elevation { get; }
-    TnTBorderRadius? BorderRadius { get; }
-}
 
+/// <summary>
+///     Interface representing styleable components with various styling properties.
+/// </summary>
+public interface ITnTStyleable {
+
+    /// <summary>
+    ///     Gets the background color.
+    /// </summary>
+    TnTColor BackgroundColor { get; }
+
+    /// <summary>
+    ///     Gets the border radius.
+    /// </summary>
+    TnTBorderRadius? BorderRadius { get; }
+
+    /// <summary>
+    ///     Gets the elevation level.
+    /// </summary>
+    int Elevation { get; }
+
+    /// <summary>
+    ///     Gets the text alignment.
+    /// </summary>
+    TextAlign? TextAlignment { get; }
+
+    /// <summary>
+    ///     Gets the text color.
+    /// </summary>
+    TnTColor TextColor { get; }
+}
