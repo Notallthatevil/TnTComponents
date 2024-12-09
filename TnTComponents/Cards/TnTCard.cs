6433e98ef003b89a5e40e13d4df3c3bae77d94ef
Nathan Tripp <ntripp@workfirstcasualty.com>
2024-12-09 14:52:48 -0700

    Improve TnTCard documentation and property organization
    Updated TnTCard class to include XML documentation comments for
    better understanding. Reordered TextAlignment, TextColor, and
    Elevation properties. Added XML documentation to ChildContent
    and Appearance properties. Updated ElementClass and ElementStyle
    to use CssClassBuilder and CssStyleBuilder respectively.

--- TnTComponents/Cards/TnTCard.cs
+++ TnTComponents/Cards/TnTCard.cs
@@ -5,31 +5,49 @@ using TnTComponents.Interfaces;
 
 namespace TnTComponents;
 
+/// <summary>
+///     Represents a card component with customizable appearance, background color, border radius,
+///     elevation, text alignment, and text color.
+/// </summary>
 public class TnTCard : TnTComponentBase, ITnTStyleable {
+
+    /// <summary>
+    ///     Gets or sets the appearance of the card. Default is <see cref="CardAppearance.Filled" />.
+    /// </summary>
     [Parameter]
     public CardAppearance Appearance { get; set; } = CardAppearance.Filled;
-    [Parameter]
-    public TextAlign? TextAlignment { get; set; }
+
     [Parameter]
     public TnTColor BackgroundColor { get; set; } = TnTColor.SurfaceContainerLow;
-    [Parameter]
-    public TnTColor TextColor { get; set; } = TnTColor.OnSurface;
-    [Parameter]
-    public int Elevation { get; set; } = 1;
+
     [Parameter]
     public TnTBorderRadius? BorderRadius { get; set; } = new(3);
+
+    /// <summary>
+    ///     Gets or sets the child content to be rendered inside the card.
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
     public override string? ElementClass => CssClassBuilder.Create()
         .AddFromAdditionalAttributes(AdditionalAttributes)
         .AddTnTStyleable(this)
         .AddFilled(Appearance == CardAppearance.Filled)
         .AddOutlined(Appearance == CardAppearance.Outlined)
         .Build();
+
     public override string? ElementStyle => CssStyleBuilder.Create()
         .AddFromAdditionalAttributes(AdditionalAttributes)
         .Build();
 
     [Parameter]
-    public RenderFragment ChildContent { get; set; } = default!;
+    public int Elevation { get; set; } = 1;
+
+    [Parameter]
+    public TextAlign? TextAlignment { get; set; }
+
+    [Parameter]
+    public TnTColor TextColor { get; set; } = TnTColor.OnSurface;
 
     protected override void BuildRenderTree(RenderTreeBuilder builder) {
         builder.OpenElement(0, "div");
