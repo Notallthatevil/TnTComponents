3cb6fc5da1adde7138192f1a29594bbe3eb03978
Nate Tripp <nate.tripp13448@gmail.com>
2024-12-11 21:30:48 -0700

    Improve XML documentation and add JSInterop usage
    Updated XML documentation comments for better readability by adding extra spaces. Added the `using Microsoft.JSInterop;` directive to enable JavaScript interop features in the `TnTExternalClickHandler` class.

--- TnTComponents/Core/TnTExternalClickHandler.razor.cs
+++ TnTComponents/Core/TnTExternalClickHandler.razor.cs
@@ -5,12 +5,12 @@ using System.Diagnostics.CodeAnalysis;
 namespace TnTComponents.Core;
 
 /// <summary>
-/// A component that handles external click events.
+///     A component that handles external click events.
 /// </summary>
 public partial class TnTExternalClickHandler {
 
     /// <summary>
-    /// Gets or sets the child content to be rendered inside this component.
+    ///     Gets or sets the child content to be rendered inside this component.
     /// </summary>
     [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
@@ -25,7 +25,7 @@ public partial class TnTExternalClickHandler {
         .Build();
 
     /// <summary>
-    /// Gets or sets the callback to be invoked when an external click is detected.
+    ///     Gets or sets the callback to be invoked when an external click is detected.
     /// </summary>
     [Parameter, EditorRequired]
     public EventCallback ExternalClickCallback { get; set; }
@@ -33,21 +33,21 @@ public partial class TnTExternalClickHandler {
     public override string? JsModulePath => "./_content/TnTComponents/Core/TnTExternalClickHandler.razor.js";
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="TnTExternalClickHandler"/> class.
+    ///     Initializes a new instance of the <see cref="TnTExternalClickHandler" /> class.
     /// </summary>
     [DynamicDependency(nameof(OnClick))]
     public TnTExternalClickHandler() { }
 
     /// <summary>
-    /// Invoked by JavaScript to handle an external click event.
+    ///     Invoked by JavaScript to handle an external click event.
     /// </summary>
     [JSInvokable]
     public async Task OnClick() => await ExternalClickCallback.InvokeAsync();
 
     /// <summary>
-    /// Disposes the component and deregisters the external click callback.
+    ///     Disposes the component and deregisters the external click callback.
     /// </summary>
-    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+    /// <returns>A <see cref="ValueTask" /> representing the asynchronous operation.</returns>
     protected override async ValueTask DisposeAsyncCore() {
         if (IsolatedJsModule is not null) {
             await IsolatedJsModule.InvokeVoidAsync("externalClickCallbackDeregister", DotNetObjectRef).ConfigureAwait(false);
@@ -56,10 +56,10 @@ public partial class TnTExternalClickHandler {
     }
 
     /// <summary>
-    /// Called after the component has been rendered.
+    ///     Called after the component has been rendered.
     /// </summary>
     /// <param name="firstRender">True if this is the first time the component is being rendered.</param>
-    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    /// <returns>A <see cref="Task" /> representing the asynchronous operation.</returns>
     protected override async Task OnAfterRenderAsync(bool firstRender) {
         await base.OnAfterRenderAsync(firstRender);
         if (firstRender && IsolatedJsModule is not null) {
