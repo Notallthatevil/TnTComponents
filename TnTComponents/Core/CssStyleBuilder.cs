a55616a64a7dc3e62a0eeeddfc27c91a6892cd9d
Nate Tripp <nate.tripp13448@gmail.com>
2024-12-11 21:22:36 -0700

    Add XML documentation to CssStyleBuilder class
    This commit introduces XML documentation comments for the
    `CssStyleBuilder` class and its methods. The added comments
    provide summaries and details about parameters and return
    values, enhancing code readability and helping developers
    understand the purpose and usage of the class more easily.

--- TnTComponents/Core/CssStyleBuilder.cs
+++ TnTComponents/Core/CssStyleBuilder.cs
@@ -2,14 +2,26 @@
 
 namespace TnTComponents.Core;
 
+/// <summary>
+///     A builder class for constructing CSS style strings.
+/// </summary>
 internal class CssStyleBuilder {
     private readonly Dictionary<string, string> _styles = [];
 
     private CssStyleBuilder() {
     }
 
+    /// <summary>
+    ///     Creates a new instance of <see cref="CssStyleBuilder" />.
+    /// </summary>
+    /// <returns>A new instance of <see cref="CssStyleBuilder" />.</returns>
     public static CssStyleBuilder Create() => new();
 
+    /// <summary>
+    ///     Adds styles from additional attributes if they contain a "style" key.
+    /// </summary>
+    /// <param name="additionalAttributes">A dictionary of additional attributes.</param>
+    /// <returns>The current instance of <see cref="CssStyleBuilder" />.</returns>
     public CssStyleBuilder AddFromAdditionalAttributes(IReadOnlyDictionary<string, object>? additionalAttributes) {
         if (additionalAttributes?.TryGetValue("style", out var style) == true && style is not null) {
             return AddStyle(style.ToString(), string.Empty);
@@ -17,6 +29,13 @@ internal class CssStyleBuilder {
         return this;
     }
 
+    /// <summary>
+    ///     Adds a CSS style to the builder.
+    /// </summary>
+    /// <param name="key">    The CSS property name.</param>
+    /// <param name="value">  The CSS property value.</param>
+    /// <param name="enabled">A flag indicating whether the style should be added.</param>
+    /// <returns>The current instance of <see cref="CssStyleBuilder" />.</returns>
     public CssStyleBuilder AddStyle(string? key, string? value, bool enabled = true) {
         if (!string.IsNullOrWhiteSpace(key) && value is not null && enabled) {
             _styles[key] = value;
@@ -24,6 +43,13 @@ internal class CssStyleBuilder {
         return this;
     }
 
+    /// <summary>
+    ///     Adds a CSS variable to the builder.
+    /// </summary>
+    /// <param name="varName"> The name of the CSS variable.</param>
+    /// <param name="varValue">The value of the CSS variable.</param>
+    /// <param name="enabled"> A flag indicating whether the variable should be added.</param>
+    /// <returns>The current instance of <see cref="CssStyleBuilder" />.</returns>
     public CssStyleBuilder AddVariable(string varName, string varValue, bool enabled = true) {
         if (enabled) {
             return AddStyle($"--{varName}", varValue);
@@ -33,6 +59,13 @@ internal class CssStyleBuilder {
         }
     }
 
+    /// <summary>
+    ///     Adds a CSS variable to the builder with a color value.
+    /// </summary>
+    /// <param name="varName">The name of the CSS variable.</param>
+    /// <param name="color">  The color value for the CSS variable.</param>
+    /// <param name="enabled">A flag indicating whether the variable should be added.</param>
+    /// <returns>The current instance of <see cref="CssStyleBuilder" />.</returns>
     public CssStyleBuilder AddVariable(string varName, TnTColor color, bool enabled = true) {
         if (enabled) {
             return AddStyle($"--{varName}", $"var(--tnt-color-{color.ToCssClassName()})");
@@ -42,6 +75,10 @@ internal class CssStyleBuilder {
         }
     }
 
+    /// <summary>
+    ///     Builds the CSS style string.
+    /// </summary>
+    /// <returns>A CSS style string or null if no styles were added.</returns>
     public string? Build() {
         var styles = _styles.Where(kv => !string.IsNullOrWhiteSpace(kv.Key));
         if (styles.Any()) {
