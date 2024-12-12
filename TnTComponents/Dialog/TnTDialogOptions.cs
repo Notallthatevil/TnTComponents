a9016c2b0a40409ab66422ceb3cd54f619801c87
Nate Tripp <nate.tripp13448@gmail.com>
2024-12-12 09:01:46 -0700

    Enhance TnTDialogOptions with documentation and properties
    Updated the `TnTDialogOptions` class to include XML documentation comments for improved readability.
    Reorganized properties and added `CloseOnExternalClick` and `ShowCloseButton` to enhance functionality.
    The `ElementClass` and `ElementStyle` properties were documented to clarify their roles as custom CSS class and style for the dialog element.

--- TnTComponents/Dialog/TnTDialogOptions.cs
+++ TnTComponents/Dialog/TnTDialogOptions.cs
@@ -3,16 +3,45 @@ using TnTComponents.Interfaces;
 
 namespace TnTComponents;
 
+/// <summary>
+///     Represents the options for configuring a TnT dialog component.
+/// </summary>
 public class TnTDialogOptions : ITnTStyleable {
-    public TextAlign? TextAlignment { get; }
     public TnTColor BackgroundColor { get; init; } = TnTColor.SurfaceContainerHighest;
-    public TnTColor TextColor { get; init; } = TnTColor.OnSurface;
-    public int Elevation { get; init; } = 2;
     public TnTBorderRadius? BorderRadius { get; init; } = new(2);
-    public string? ElementStyle { get; init; }
-    public string? ElementClass { get; init; }
+
+    /// <summary>
+    ///     Gets a value indicating whether the dialog should close when clicking outside of it.
+    /// </summary>
     public bool CloseOnExternalClick { get; init; } = true;
+
+    /// <summary>
+    ///     Gets the custom CSS class for the dialog element.
+    /// </summary>
+    public string? ElementClass { get; init; }
+
+    /// <summary>
+    ///     Gets the custom style for the dialog element.
+    /// </summary>
+    public string? ElementStyle { get; init; }
+
+    public int Elevation { get; init; } = 2;
+
+    /// <summary>
+    ///     Gets a value indicating whether the dialog should show a close button.
+    /// </summary>
     public bool ShowCloseButton { get; init; } = true;
+
+    public TextAlign? TextAlignment { get; }
+    public TnTColor TextColor { get; init; } = TnTColor.OnSurface;
+
+    /// <summary>
+    ///     Gets the title of the dialog.
+    /// </summary>
     public string? Title { get; init; }
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether the dialog is in the process of closing.
+    /// </summary>
     internal bool Closing { get; set; }
 }
