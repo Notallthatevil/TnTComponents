ef58fead54654c0256b6a68b82d6abe470a9e93c
Nate Tripp <nate.tripp13448@gmail.com>
2024-12-20 11:32:30 -0700

    Add XML documentation to TnTPageScript component
    Enhanced code readability by adding XML documentation comments to the TnTPageScript class and its Src property. This provides a clear summary of the component's purpose and details about the Src parameter, improving understanding for developers.

--- TnTComponents/Script/TnTPageScript.cs
+++ TnTComponents/Script/TnTPageScript.cs
@@ -3,11 +3,18 @@ using Microsoft.AspNetCore.Components.Rendering;
 
 namespace TnTComponents;
 
+/// <summary>
+///     A Blazor component that renders a custom script element with a specified source.
+/// </summary>
 public class TnTPageScript : ComponentBase {
 
+    /// <summary>
+    ///     Gets or sets the source URL of the script.
+    /// </summary>
     [Parameter, EditorRequired]
     public string Src { get; set; } = default!;
 
+    /// <inheritdoc />
     protected override void BuildRenderTree(RenderTreeBuilder builder) {
         builder.OpenElement(0, "tnt-page-script");
         builder.AddAttribute(1, "src", Src);
