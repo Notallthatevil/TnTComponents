2d6f808fd57375ebd169d39b0741d51f71a1bb0b
Nate Tripp <nate.tripp13448@gmail.com>
2025-01-21 12:34:02 -0700

    Refactor IQueryable extensions and update project files
    - Updated `IQueryableExt.cs` to include additional using directives and changed namespace to `TnTComponents.Extensions`.
    - Converted `TnTComponents.Extensions.csproj` to SDK-style format with multiple target frameworks and added project reference.
    - Integrated `TnTComponents.Extensions` project into the solution, updating the solution file to include necessary configurations for building.

--- TnTComponents.Extensions/IQueryableExt.cs
+++ TnTComponents.Extensions/IQueryableExt.cs
@@ -1,7 +1,12 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
 using TnTComponents.Virtualization;
 
-namespace TnTComponents.Ext;
+namespace TnTComponents.Extensions;
 
 public static class IQueryableExt {
 
