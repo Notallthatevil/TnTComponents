c41a7004e6f96de14714319d899c03f71d58de89
Nate Tripp <nate.tripp13448@gmail.com>
2025-06-09 15:26:27 -0600

    Add unit tests for TnTButton component and setup JS interop

---
+++ TnTComponents.Tests/Ext/BunitJSInteropExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnTComponents.Tests.Ext;
+public static class BunitJSInteropExt {
+
+    public static void SetupDefaultModule(this BunitJSInterop jsInterop, string path) {
+        var module = jsInterop.SetupModule(path);
+        module.SetupVoid("onLoad", _ => true);
+        module.SetupVoid("onUpdate", _ => true);
+        module.SetupVoid("onDispose", _ => true);
+    }
+}
