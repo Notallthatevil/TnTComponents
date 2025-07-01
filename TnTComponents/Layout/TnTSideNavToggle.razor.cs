3f49e8d96e1dd66fefb8ed2ae0792d39aa837373
Nate Tripp <nate.tripp13448@gmail.com>
2025-06-30 19:50:07 -0600

    Enhance side navigation and toggle button functionality
    - Updated `TnTLayout.razor.scss` to use `::deep` for styling `.tnt-side-nav` elements, improving specificity and removing unused styles.
    - Changed side navigation width in `TnTSideNav.razor.scss` to `measurements.$tnt-side-nav-width` for standardization.
    - Refactored `TnTSideNavToggle.razor` to include namespace and using directives, enhancing code organization and functionality.
    - Modified `TnTSideNavToggle.razor.cs` to be a partial class and simplified the `Icon` property initialization.
    - Removed `BuildRenderTree` method in `TnTSideNavToggle.razor.cs`, shifting to a more declarative approach.
    - Added styles for the toggle button in `TnTSideNavToggle.razor.scss`, improving appearance and interactivity.

--- TnTComponents/Layout/TnTSideNavToggle.razor.cs
+++ TnTComponents/Layout/TnTSideNavToggle.razor.cs
@@ -10,7 +10,7 @@ namespace TnTComponents;
 /// <summary>
 ///     Toggle button for the side navigation component.
 /// </summary>
-public class TnTSideNavToggle : TnTComponentBase, ITnTInteractable {
+public partial class TnTSideNavToggle {
 
     /// <inheritdoc />
     [Parameter]
@@ -40,7 +40,7 @@ public class TnTSideNavToggle : TnTComponentBase, ITnTInteractable {
     ///     The icon to be displayed in the toggle button.
     /// </summary>
     [Parameter]
-    public TnTIcon Icon { get; set; } = new MaterialIcon { Icon = MaterialIcon.Menu };
+    public TnTIcon Icon { get; set; } = MaterialIcon.Menu;
 
     /// <inheritdoc />
     [Parameter]
@@ -50,24 +50,5 @@ public class TnTSideNavToggle : TnTComponentBase, ITnTInteractable {
     [Parameter]
     public TnTColor? TintColor { get; set; } = TnTColor.SurfaceTint;
 
-    /// <inheritdoc />
-    protected override void BuildRenderTree(RenderTreeBuilder builder) {
-        builder.OpenElement(0, "button");
-        builder.AddMultipleAttributes(10, AdditionalAttributes);
-        builder.AddAttribute(20, "class", ElementClass);
-        builder.AddAttribute(30, "style", ElementStyle);
-        builder.AddAttribute(40, "lang", ElementLang);
-        builder.AddAttribute(50, "id", ElementId);
-        builder.AddAttribute(60, "title", ElementTitle);
-        builder.AddAttribute(70, "onclick", "TnTComponents.toggleSideNav(event)");
-        builder.AddElementReferenceCapture(80, e => Element = e);
-        builder.AddContent(90, Icon.Render());
 
-        if (EnableRipple) {
-            builder.OpenComponent<TnTRippleEffect>(100);
-            builder.CloseComponent();
-        }
-
-        builder.CloseElement();
-    }
 }
