8bbd838a1cf2da61808c61d2339f03381ac2cb02
Nate Tripp <nate.tripp13448@gmail.com>
2025-07-03 09:09:47 -0600

    Refactor TnTInputCheckbox and TnTInputSwitch styles
    - Updated grid template areas in TnTInputCheckbox to use "checkbox" instead of "text".
    - Introduced a new structure in TnTInputSwitch with labels and icons.
    - Modified TnTInputSwitch to be independent from TnTInputCheckbox.
    - Expanded SCSS for TnTInputSwitch with new styles for hover, focus, and error handling.
    - Standardized the use of color and typescale variables for better maintainability.

---
+++ TnTComponents/Form/TnTInputSwitch.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using TnTComponents.Core;
+
+namespace TnTComponents;
+
+/// <summary>
+///     Represents a switch input component.
+/// </summary>
+public partial class TnTInputSwitch {
+
+    /// <inheritdoc />
+    public override InputType Type => InputType.Checkbox;
+
+    /// <inheritdoc />
+    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out bool result, [NotNullWhen(false)] out string? validationErrorMessage) {
+        throw new NotSupportedException();
+    }
+}
