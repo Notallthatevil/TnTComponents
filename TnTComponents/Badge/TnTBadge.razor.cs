1a3e7716b23be6008ea4635487c26daabcd6f87f
Nate Tripp <nate.tripp13448@gmail.com>
2025-07-08 12:10:46 -0600

    Enhance button and badge components for better UX
    - Added multiple button sizes (XS to Largest) with icons and badges.
    - Introduced progress indicators in new button rows.
    - Removed `ITnTStyleable` interface and simplified `TnTBadge`.
    - Reduced parameters in `CreateBadge` method for ease of use.
    - Added new styles for badges in `TnTBadge.razor.scss`.
    - Updated button styles in `TnTButton.razor.scss` for consistency.
    - Enhanced interactive styles in `_interactable.scss`.

--- TnTComponents/Badge/TnTBadge.razor.cs
+++ TnTComponents/Badge/TnTBadge.razor.cs
@@ -16,28 +16,20 @@ public partial class TnTBadge {
 
     /// <inheritdoc />
     [Parameter]
-    public TnTBorderRadius? BorderRadius { get; set; } = TnTBorderRadius.Full;
-
-    /// <inheritdoc />
-    [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
 
     /// <inheritdoc />
     public override string? ElementClass => CssClassBuilder.Create()
         .AddFromAdditionalAttributes(AdditionalAttributes)
         .AddClass("tnt-badge")
-        .AddFilled()
-        .AddTnTStyleable(this)
         .Build();
 
     /// <inheritdoc />
     public override string? ElementStyle => CssStyleBuilder.Create()
         .AddFromAdditionalAttributes(AdditionalAttributes)
+        .AddVariable("tnt-badge-background-color", BackgroundColor)
+        .AddVariable("tnt-badge-text-color", TextColor)
         .Build();
-
-    /// <inheritdoc />
-    [Parameter]
-    public int Elevation { get; set; } = 2;
 
     /// <inheritdoc />
     [Parameter]
@@ -57,29 +49,12 @@ public partial class TnTBadge {
     /// </summary>
     /// <param name="content">        The content to be displayed inside the badge.</param>
     /// <param name="backgroundColor">The background color of the badge.</param>
-    /// <param name="elevation">      The elevation level of the badge.</param>
     /// <param name="textAlignment">  The text alignment within the badge.</param>
     /// <param name="textColor">      The text color of the badge.</param>
     /// <param name="cssClass">       Additional CSS classes for the badge.</param>
     /// <param name="cssStyle">       Additional CSS styles for the badge.</param>
     /// <returns>A new instance of <see cref="TnTBadge" />.</returns>
-    public static TnTBadge CreateBadge(string content, TnTColor backgroundColor = TnTColor.Error, int elevation = 2, TextAlign? textAlignment = TextAlign.Center, TnTColor textColor = TnTColor.OnError, string? cssClass = null, string? cssStyle = null) {
-        return CreateBadge(content, TnTBorderRadius.Full, backgroundColor, elevation, textAlignment, textColor, cssClass, cssStyle);
-    }
-
-    /// <summary>
-    ///     Creates a new instance of <see cref="TnTBadge" /> with specified content and properties.
-    /// </summary>
-    /// <param name="content">        The content to be displayed inside the badge.</param>
-    /// <param name="borderRadius">   The border radius of the badge.</param>
-    /// <param name="backgroundColor">The background color of the badge.</param>
-    /// <param name="elevation">      The elevation level of the badge.</param>
-    /// <param name="textAlignment">  The text alignment within the badge.</param>
-    /// <param name="textColor">      The text color of the badge.</param>
-    /// <param name="cssClass">       Additional CSS classes for the badge.</param>
-    /// <param name="cssStyle">       Additional CSS styles for the badge.</param>
-    /// <returns>A new instance of <see cref="TnTBadge" />.</returns>
-    public static TnTBadge CreateBadge(string content, TnTBorderRadius? borderRadius, TnTColor backgroundColor = TnTColor.Error, int elevation = 2, TextAlign? textAlignment = TextAlign.Center, TnTColor textColor = TnTColor.OnError, string? cssClass = null, string? cssStyle = null) {
+    public static TnTBadge CreateBadge(string content, TnTColor backgroundColor = TnTColor.Error, TextAlign? textAlignment = TextAlign.Center, TnTColor textColor = TnTColor.OnError, string? cssClass = null, string? cssStyle = null) {
         Dictionary<string, object> additionalAttributes = [];
         if (!string.IsNullOrWhiteSpace(cssClass)) {
             additionalAttributes.Add("class", cssClass);
@@ -90,11 +65,9 @@ public partial class TnTBadge {
         }
         return new TnTBadge {
             BackgroundColor = backgroundColor,
-            BorderRadius = borderRadius,
             ChildContent = new RenderFragment(builder => {
                 builder.AddContent(0, content);
             }),
-            Elevation = elevation,
             TextAlignment = textAlignment,
             TextColor = textColor,
             AdditionalAttributes = additionalAttributes
