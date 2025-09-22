58c34398baa844dd89a23cf03c866431e9ce89c8
Nate Tripp <nate.tripp13448@gmail.com>
2025-09-22 14:03:24 -0600

    Add ExcludeFromCodeCoverage to BrowserStorageDisabledException
    Added the `using System.Diagnostics.CodeAnalysis;` directive to enable the use of code analysis attributes. Marked the `BrowserStorageDisabledException` class with the `[ExcludeFromCodeCoverage]` attribute to exclude it from code coverage analysis, as it is not relevant for testing coverage metrics.

--- TnTComponents/Storage/BrowserStorageDisabledException.cs
+++ TnTComponents/Storage/BrowserStorageDisabledException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace TnTComponents.Storage;
 /// <summary>
 /// Exception thrown when browser storage is disabled.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public class BrowserStorageDisabledException : Exception {
     /// <summary>
     /// Initializes a new instance of the <see cref="BrowserStorageDisabledException"/> class.
